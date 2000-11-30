ed6afebdc2fe4d448961511527dc7fb3521d4488
dwhedon <dwhedon@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2000-11-30 07:53:49 +0000

    ethdetect is smaller, by a few k, unclear whether this was worth it. Using our own struct to store the card info.  We build a libdetect compatible struct at runtime. May be having trouble with ISA cards now, not sure.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@174 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/ddetect/ddetect.h
+++ tools/ddetect/ddetect.h
@@ -1,3 +1,25 @@
+#include <sys/types.h>
+
+
+typedef struct {
+        u_int32_t long_id;
+} pci_ids_t;
+
+
+typedef struct {
+        char *name;
+        pci_ids_t *id;
+} pci_module_list_t;
+
+
+typedef struct {
+       char *dev_id;
+} isa_dev_id_t;
+
+typedef struct {
+    char *name;
+    isa_dev_id_t *id;
+} isa_module_list_t;
 
 
 extern int passive_detection;
--- tools/ddetect/ethdetect.c
+++ tools/ddetect/ethdetect.c
@@ -6,30 +6,79 @@
 
 
 #include <stdio.h>
-#include <detect.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <detect.h>
 
-#include "ethdetect.h"
 #include "ddetect.h"
+#include "ethdetect.h"
 
 int
 main (int argc, char *argv[])
 {
   struct bus_lst bus = { 0 };
   struct ethernet_info *ethernet = (struct ethernet_info *) NULL;
-  int i = 0;
+  int i, j, num_cards;
+
+  struct cards_lst *lst = NULL;
+  char *unknown_str = "unknown";
+  char *ignore_str = "ignore";
 
   ddetect_getopts (argc, argv);
+  num_cards = 0;
+  for (i = 0; pci_modules[i].name; i++)
+    for (j = 0; pci_modules[i].id[j].long_id; j++)
+      num_cards++;
+
+  if (!passive_detection)
+    {
+      for (i = 0; isa_modules[i].name; i++)
+	for (j = 0; isa_modules[i].id[j].dev_id; j++)
+	  num_cards++;
+    }
+
+  lst = malloc (sizeof (struct cards_lst) * num_cards);
+
+  num_cards = 0;
+
+  /* initialize list of pci cards */
+  for (i = 0; pci_modules[i].name; i++)
+    for (j = 0; pci_modules[i].id[j].long_id; j++)
+      {
+	lst[num_cards].vendor = unknown_str;
+	lst[num_cards].model = unknown_str;
+	lst[num_cards].modulename = pci_modules[i].name;
+	lst[num_cards].bus = PCI;
+	lst[num_cards].dev_id = ignore_str;
+	lst[num_cards].long_id = pci_modules[i].id[j].long_id;
+	lst[num_cards].type = ETHERNETCARD;
+	lst[num_cards].options = 0;
+	lst[num_cards].next = &lst[++num_cards];
+      }
+
+
+  if (!passive_detection)
+    {
+      /* initialize list of isa cards */
+      for (i = 0; isa_modules[i].name; i++)
+	for (j = 0; isa_modules[i].id[j].dev_id; j++)
+	  {
+	    lst[num_cards].vendor = unknown_str;
+	    lst[num_cards].model = unknown_str;
+	    lst[num_cards].modulename = isa_modules[i].name;
+	    lst[num_cards].bus = ISA;
+	    lst[num_cards].dev_id = isa_modules[i].id[j].dev_id;
+	    lst[num_cards].long_id = 0;
+	    lst[num_cards].type = ETHERNETCARD;
+	    lst[num_cards].options = 0;
+	    lst[num_cards].next = &lst[++num_cards];
+	  }
+    }
+
+  lst[num_cards - 1].next = NULL;
+
 
   sync ();
-
-  while (lst[i].vendor)
-    {
-      lst[i].next = &lst[++i];
-    }
-  lst[i - 1].next = NULL;
-
-
   bus.pci = pci_detect (lst);
   if (!passive_detection)
     bus.isa = isa_detect (lst);
@@ -39,15 +88,13 @@ main (int argc, char *argv[])
 
   if (((ethernet = ethernet_detect (&bus)) != NULL) && (debug == 1))
     {
-      printf ("\nbus:module\n");
-    }				/*endif */
+      printf ("\nbus:module:(id)\n");
+    }
   for (; ethernet; ethernet = ethernet->next)
     {
-      printf ("%s:%s\n",
-	      bus2str (ethernet->bus),
-	      ethernet->module);
-    }				/*next ethernet */
-
+      printf ("%s:%s:(0x%.8lx)\n",
+	      bus2str (ethernet->bus), ethernet->module, ethernet->long_id);
+    }
 
 
   return 0;
