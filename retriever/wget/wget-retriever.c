ca4b06010fcbe1f8271554ba474a184faca22f61
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2000-12-07 21:34:05 +0000

    no longer supports retreiving to stdout
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@211 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- retriever/wget/wget-retriever.c
+++ retriever/wget/wget-retriever.c
@@ -16,23 +16,18 @@
 
 int main(int argc, char **argv) {
 	int ret;
-	char *hostname, *directory, *dest, *command;
+	char *hostname, *directory, *command;
 					    // TODO: what about ftp?
 	debconf_command("GET", DEBCONF_BASE "http/hostname", NULL);
 	hostname=strdup(debconf_ret());
 	debconf_command("GET", DEBCONF_BASE "http/directory", NULL);
 	directory=debconf_ret();
 	
-	if (argc < 3)
-		dest="-"; /* stdout */
-	else
-		dest=argv[2];
-	
 	command=malloc( 20 /* wget --quiet http:// */ + strlen(hostname) +
 			strlen(directory) + 1 /* / */ + strlen(argv[1]) +
-			4 /*  -O  */ + strlen(dest) + 1);
+			4 /*  -O  */ + strlen(argv[2]) + 1);
 	sprintf(command, "wget --quiet http://%s%s/%s -O %s", hostname,
-			directory, argv[1], dest);
+			directory, argv[1], argv[2]);
 	ret=system(command);
 	if (ret == 256)
 		return 1;
