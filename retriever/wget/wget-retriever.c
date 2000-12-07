59c6b0d5c6d7e3cf16966650dcf5a8491994a67d
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2000-12-07 23:41:09 +0000

    added a path to the Packages file
    Added a DEMO define, that will point it at
    http://people.debian.org/~joeyh/debian-installer/udebs/
    since we still don't have a real udeb repository
    
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@216 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- retriever/wget/wget-retriever.c
+++ retriever/wget/wget-retriever.c
@@ -13,9 +13,11 @@
 #include "debconf.h"
 
 #define DEBCONF_BASE "mirror/"
+#define DEMO 1
 
 int main(int argc, char **argv) {
 	int ret;
+	char *src;
 	char *hostname, *directory, *command;
 					    // TODO: what about ftp?
 	debconf_command("GET", DEBCONF_BASE "http/hostname", NULL);
@@ -23,11 +25,38 @@ int main(int argc, char **argv) {
 	debconf_command("GET", DEBCONF_BASE "http/directory", NULL);
 	directory=debconf_ret();
 	
+#ifdef DEMO
+	/*
+	 * For the demo system, ignore this, and use the only repository we 
+	 * have.
+	 */
+	hostname="people.debian.org";
+	directory="/~joeyh/debian-installer/udebs";
+#endif
+	
+	src=argv[1];
+	/*
+	 * Intercept requests for a Packages file, and
+	 * add the path to Packages files in the mirror.
+	 * (All other files will be relative to the mirror 
+	 * root directory.)
+	 */
+	if (strcmp(src, "Packages") == 0) {
+#ifndef DEMO
+		/* TODO: obviously this path is woody and i386 specific. FIXME */
+		/* It's also probably not right.. */
+		src="dists/woody/main/installer-i386/Packages";
+#else
+		/* For the demo system, I just hardcode this. */
+		src="Packages";
+#endif
+	}
+	
 	command=malloc( 20 /* wget --quiet http:// */ + strlen(hostname) +
-			strlen(directory) + 1 /* / */ + strlen(argv[1]) +
+			strlen(directory) + 1 /* / */ + strlen(src) +
 			4 /*  -O  */ + strlen(argv[2]) + 1);
 	sprintf(command, "wget --quiet http://%s%s/%s -O %s", hostname,
-			directory, argv[1], argv[2]);
+			directory, src, argv[2]);
 	ret=system(command);
 	if (ret == 256)
 		return 1;
