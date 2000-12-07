e62a5882667bf95ce458b2aa653fe4cd975bf788
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2000-12-07 00:06:35 +0000

    converted it to using a jump table of functions in the state machine.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@195 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- retriever/choose-mirror/choose-mirror.c
+++ retriever/choose-mirror/choose-mirror.c
@@ -1,8 +1,5 @@
 /*
  * Mirror selection via debconf.
- * TODO: don't use DEBCONF_BASE like I do, that wastes space. Instead,
- *       make a function that appends it or something? Function shouldn't
- *       malloc... static buffer?
  */
 
 #include <string.h>
@@ -55,66 +52,73 @@ char *mirror_root(char *mirror) {
 	return NULL;
 }
 
-int main (int argc, char **argv) {
-	int manual_entry;
+void choose_country(void) {
+	char *list=debconf_list(countries_http);
+	debconf_command("SUBST", DEBCONF_BASE "country", "countries", list, NULL);
+	free(list);
+	debconf_command("INPUT", "high", DEBCONF_BASE "country", NULL);
+}
+
+int manual_entry;
+
+void choose_mirror(void) {
 	char *list;
+
+	debconf_command("GET", DEBCONF_BASE "country", NULL);
+	manual_entry = ! strcmp(debconf_ret(), "enter information manually");
+	if (! manual_entry) {
+		/* Prompt for mirror in selected country. */
+		list=debconf_list(mirrors_in(debconf_ret()));
+		debconf_command("SUBST", DEBCONF_BASE "http/mirror", "mirrors", list, NULL);
+		free(list);
+		debconf_command("INPUT", "medium", DEBCONF_BASE "http/mirror", NULL);
+	}
+	else {
+		/* Manual entry. */
+		debconf_command("INPUT", "critical", DEBCONF_BASE "http/hostname", NULL);
+		debconf_command("INPUT", "critical", DEBCONF_BASE "http/directory", NULL);
+	}
+	/* Always ask about a proxy. */
+	debconf_command("INPUT", "high", DEBCONF_BASE "http/proxy", NULL);
+}
+
+void validate_mirror(void) {
 	char *mirror;
-	int state = 1;
+
+	if (! manual_entry) {
+		/*
+		 * Copy information about the selected
+		 * mirror into mirror/http/{hostname,directory},
+		 * which is the standard location other
+		 *tools can look at.
+		 */
+		debconf_command("GET", DEBCONF_BASE "http/mirror", NULL);
+		mirror=debconf_ret();
+		debconf_command("SET", DEBCONF_BASE "http/hostname", mirror, NULL);
+		debconf_command("SET", DEBCONF_BASE "http/directory",
+			mirror_root(mirror), NULL);
+	}
+}
+
+int main (int argc, char **argv) {
+	/* Use a state machine with a function to run in each state */
+	int state = 0;
+	void (*states[])() = {
+		choose_country,
+		choose_mirror,
+		validate_mirror,
+		NULL,
+	};
 	
 	debconf_command("CAPB", "backup", NULL);
 	debconf_command("TITLE", "Choose mirror", NULL); //TODO: i18n
-	
-	/* Use a state machine to allow jumping back to previous questions. */
-	while (state != 0 && state != 3) {
-		switch (state) {
-		case 1:
-			/* Prompt for the country. */
-			list=debconf_list(countries_http);
-			debconf_command("SUBST", DEBCONF_BASE "country",
-				"countries", list, NULL);
-			free(list);
-			debconf_command("INPUT", "high", DEBCONF_BASE "country", NULL);
-			break;
-		case 2:
-			debconf_command("GET", DEBCONF_BASE "country", NULL);
-			manual_entry = ! strcmp(debconf_ret(), "enter information manually");
-			if (! manual_entry) {
-				/* Prompt for mirror in selected country. */
-				list=debconf_list(mirrors_in(debconf_ret()));
-				debconf_command("SUBST",
-					DEBCONF_BASE "http/mirror",
-					"mirrors", list, NULL);
-				debconf_command("INPUT", "medium",
-					DEBCONF_BASE "http/mirror", NULL);
-			}
-			else {
-				/* Manual entry. */
-				debconf_command("INPUT", "critical",
-						DEBCONF_BASE "http/hostname", NULL);
-				debconf_command("INPUT", "critical",
-						DEBCONF_BASE "http/directory", NULL);
-			}
-			/* Always ask about a proxy. */
-			debconf_command("INPUT", "high", DEBCONF_BASE "http/proxy", NULL);
-			break;
-		case 3:
-			if (! manual_entry) {
-				/* 
-				 * Copy information about the selected
-				 * mirror into mirror/http/{hostname,directory},
-				 * which is the standard location other
-				 * tools can look at.
-				 */
-				debconf_command("GET", "http/mirror", NULL);
-				mirror=debconf_ret();
-				debconf_command("SET", "http/hostname", mirror, NULL);
-				debconf_command("SET", "http/directory",
-						mirror_root(mirror), NULL);
-			}
-			/* TODO: Check the mirror, and what else? See apt-setup */
-			break;
-		}
 
+	/*
+	 * It's a pretty brain-dead state machine though. It advances
+	 * forward and back by one state always. Enough for our purposes.
+	 */
+	while (state >= 0 && states[state]) {
+		states[state]();
 		if (debconf_command("GO", NULL) == 0)
 			state++;
 		else
