f9fbdfa68a6e7b852df71fae3a22385b94299fe5
dwhedon <dwhedon@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2000-12-28 04:55:51 +0000

    First shot at debconf ethernet detection and module loading, not working yet.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@287 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/ddetect/ethdetect.c
+++ tools/ddetect/ethdetect.c
@@ -9,93 +9,157 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <detect.h>
+#include <debconfclient.h>
+#include <string.h>
 
 #include "ddetect.h"
 #include "ethdetect.h"
 
+static char buffer[128];
+static char *unknown_str = "unknown";
+static char *ignore_str = "ignore";
+static struct debconfclient *client;
+
+
+static char * debconf_input (char *priority, char *template);
+static int install_module(char *modulename);
+static int detect_and_install(int passive_detection);
+
 int
 main (int argc, char *argv[])
 {
-  struct bus_lst bus = { 0 };
-  struct ethernet_info *ethernet = (struct ethernet_info *) NULL;
-  int i, j, num_cards;
+    char *ptr=NULL;
+    int passive_detection;
+    
+    client = debconfclient_new ();
 
-  struct cards_lst *lst = NULL;
-  char *unknown_str = "unknown";
-  char *ignore_str = "ignore";
+    ptr = debconf_input ("high", "ddetect/detection_type");
 
-  ddetect_getopts (argc, argv);
-  num_cards = 0;
-  for (i = 0; pci_modules[i].name; i++)
-    for (j = 0; pci_modules[i].id[j].long_id; j++)
-      num_cards++;
-
-  if (!passive_detection)
-    {
-      for (i = 0; isa_modules[i].name; i++)
-	for (j = 0; isa_modules[i].id[j].dev_id; j++)
-	  num_cards++;
+    if (strstr(ptr, "none")) {
+	ptr = debconf_input ("high", "ethdetect/module_select");
+	if (strstr(ptr, "other"))
+	    ptr = debconf_input ("high", "ethdetect/module_prompt");
+	if(ptr)
+	    if (install_module(ptr) != 0){
+		client->command (client, "input", "high", "ethdetect/error", NULL);
+		client->command (client, "go", NULL);
+		exit(1);
+	    }
+	exit(0);
     }
 
-  lst = malloc (sizeof (struct cards_lst) * num_cards);
+    if (strstr(ptr, "full"))
+	passive_detection = 0;
+    else
+	passive_detection = 1;
+	
+    return detect_and_install(passive_detection);
 
-  num_cards = 0;
-
-  /* initialize list of pci cards */
-  for (i = 0; pci_modules[i].name; i++)
-    for (j = 0; pci_modules[i].id[j].long_id; j++)
-      {
-	lst[num_cards].vendor = unknown_str;
-	lst[num_cards].model = unknown_str;
-	lst[num_cards].modulename = pci_modules[i].name;
-	lst[num_cards].bus = PCI;
-	lst[num_cards].dev_id = ignore_str;
-	lst[num_cards].long_id = pci_modules[i].id[j].long_id;
-	lst[num_cards].type = ETHERNETCARD;
-	lst[num_cards].options = 0;
-	lst[num_cards].next = &lst[++num_cards];
-      }
+}
 
 
-  if (!passive_detection)
+static char *
+debconf_input (char *priority, char *template)
+{
+  client->command (client, "input", priority, template, NULL);
+  client->command (client, "go", NULL);
+  client->command (client, "get", template, NULL);
+  return client->value;
+}
+
+
+static int
+install_module(char *modulename){
+    char *params=NULL;
+    params = debconf_input ("high", "ethdetect/module_params");
+    snprintf(buffer, sizeof(buffer), "insmod %s %s", modulename, (params ? params : " "));
+    return system (buffer);
+}
+
+int
+detect_and_install(int passive_detection) {
+
+    struct bus_lst bus = { 0 };
+    struct ethernet_info *ethernet = (struct ethernet_info *) NULL;
+    int i, j, num_cards;
+    struct cards_lst *lst = NULL;
+    char *ptr;
+   
+    num_cards = 0;
+    for (i = 0; pci_modules[i].name; i++)
+	for (j = 0; pci_modules[i].id[j].long_id; j++)
+	    num_cards++;
+    
+    if (!passive_detection)
     {
-      /* initialize list of isa cards */
-      for (i = 0; isa_modules[i].name; i++)
-	for (j = 0; isa_modules[i].id[j].dev_id; j++)
-	  {
+       	for (i = 0; isa_modules[i].name; i++)
+    	    for (j = 0; isa_modules[i].id[j].dev_id; j++)
+      		num_cards++;
+    }
+    
+    lst = malloc (sizeof (struct cards_lst) * num_cards);
+    
+    num_cards = 0;
+    
+    /* initialize list of pci cards */
+    for (i = 0; pci_modules[i].name; i++)
+	for (j = 0; pci_modules[i].id[j].long_id; j++)
+	{
 	    lst[num_cards].vendor = unknown_str;
 	    lst[num_cards].model = unknown_str;
-	    lst[num_cards].modulename = isa_modules[i].name;
-	    lst[num_cards].bus = ISA;
-	    lst[num_cards].dev_id = isa_modules[i].id[j].dev_id;
-	    lst[num_cards].long_id = 0;
+	    lst[num_cards].modulename = pci_modules[i].name;
+	    lst[num_cards].bus = PCI;
+	    lst[num_cards].dev_id = ignore_str;
+	    lst[num_cards].long_id = pci_modules[i].id[j].long_id;
 	    lst[num_cards].type = ETHERNETCARD;
 	    lst[num_cards].options = 0;
 	    lst[num_cards].next = &lst[++num_cards];
-	  }
-    }
-
-  lst[num_cards - 1].next = NULL;
-
-
-  sync ();
-  bus.pci = pci_detect (lst);
-  if (!passive_detection)
-    bus.isa = isa_detect (lst);
-
-  bus.pcmcia = pcmcia_detect (lst);
-
-
-  if (((ethernet = ethernet_detect (&bus)) != NULL) && (debug == 1))
+	}
+    
+    
+    if (!passive_detection)
     {
-      printf ("\nbus:module:(id)\n");
+       	/* initialize list of isa cards */
+	for (i = 0; isa_modules[i].name; i++)
+	    for (j = 0; isa_modules[i].id[j].dev_id; j++)
+	    {
+		lst[num_cards].vendor = unknown_str;
+		lst[num_cards].model = unknown_str;
+		lst[num_cards].modulename = isa_modules[i].name;
+		lst[num_cards].bus = ISA;
+		lst[num_cards].dev_id = isa_modules[i].id[j].dev_id;
+		lst[num_cards].long_id = 0;
+		lst[num_cards].type = ETHERNETCARD;
+		lst[num_cards].options = 0;
+		lst[num_cards].next = &lst[++num_cards];
+	    }
     }
-  for (; ethernet; ethernet = ethernet->next)
+    
+    lst[num_cards - 1].next = NULL;
+    
+    
+    sync ();
+    bus.pci = pci_detect (lst);
+    if (!passive_detection)
+	bus.isa = isa_detect (lst);
+    
+    bus.pcmcia = pcmcia_detect (lst);
+    
+    for (; ethernet; ethernet = ethernet->next)
     {
-      printf ("%s:%s:(0x%.8lx)\n",
-	      bus2str (ethernet->bus), ethernet->module, ethernet->long_id);
+	client->command (client, "subst", "ethdetect/load_module", "bus", 
+		bus2str (ethernet->bus), NULL);
+	client->command (client, "subst", "ethdetect/load_module", "module",
+		ethernet->module, NULL);
+
+	ptr = debconf_input ("high", "ethdetect/load_module");
+	if (strstr(ptr, "true")){
+	    if (install_module(ethernet->module) != 0){
+		client->command (client, "input", "high", "ethdetect/error", NULL);
+		client->command (client, "go", NULL);
+	    }
+	}
     }
-
-
-  return 0;
+    
+    return 0;
 }
