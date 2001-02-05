49a02213e2c1379ad322efe53573cf39a84db641
dwhedon <dwhedon@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2001-02-05 07:08:40 +0000

    fix warning
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@477 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- libd-i/d-i.h
+++ libd-i/d-i.h
@@ -5,6 +5,6 @@
 #define MAXLINE 512
 
 int di_execlog (const char *incmd);
-di_log(char *msg);
-di_check_dir (const char *dirname);
+void di_log(char *msg);
+int di_check_dir (const char *dirname);
 
