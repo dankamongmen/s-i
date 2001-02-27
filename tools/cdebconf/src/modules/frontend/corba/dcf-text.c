cc90fbf4d84b7b69fb328ec74746a25f5eddd9bb
zw <zw@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2001-02-27 08:16:45 +0000

    Try to run happy with goad-browser, hmm..
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@549 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/cdebconf/src/modules/frontend/corba/dcf-text.c
+++ tools/cdebconf/src/modules/frontend/corba/dcf-text.c
@@ -2,42 +2,40 @@
  * cdebconf frontend, corba servant, text frontend main program, will
  * only work in an xterm, haha
  *
- * $Id: dcf-text.c,v 1.3 2001/02/22 09:22:39 zw Exp $
+ * $Id: dcf-text.c,v 1.4 2001/02/27 08:16:45 zw Rel $
+ */
+
+/*
+ * To test:
+ * $ xemacs dcf-text.gnorba
+ * $ GNOME_GNORBA_PATH=`pwd` goad-browser
  */
 
 #include "dcf-textimpl.c"
 
 int main(int argc, char* argv[]) {
-  CORBA_ORB                 orb;
-  CORBA_Environment	    *ev;
-  CORBA_Object 		    name_server;
-  PortableServer_POA        root_poa;
+  CORBA_ORB orb;
+  CORBA_Environment *ev;
+  PortableServer_POA root_poa;
   PortableServer_POAManager pm;
-  CosNaming_NameComponent   name_component[2] = {{"Debconf", "subcontext"},
-						 {"Frontend", "server"}};
-  CosNaming_Name 	    name = {2, 2, name_component, CORBA_FALSE};
-  Debconf_Frontend     	    dcf_text;
-  CORBA_char                *objref;
+  CORBA_Object name_service;
+  Debconf_Frontend dcf_text;
+  CORBA_char *ior;
   
   ev = g_new0(CORBA_Environment,1);
 
   CORBA_exception_init(ev);
 
-  orb = gnorba_CORBA_init(&argc, argv,
-			  GNORBA_INIT_SERVER_FUNC,
-			  ev);
+  orb = gnorba_CORBA_init(&argc, argv, GNORBA_INIT_SERVER_FUNC, ev);
   if (ev->_major != CORBA_NO_EXCEPTION) {
-    fprintf(stderr, "Error: could not initializing CORBA ORB: %s\n", 
-	    CORBA_exception_id(ev));
+    fprintf(stderr, "Error: could not initializing ORB: %s\n", CORBA_exception_id(ev));
     exit(1);
   }
 
-  root_poa = (PortableServer_POA) CORBA_ORB_resolve_initial_references(orb,
-								       "RootPOA",
-								       ev);
+  root_poa = (PortableServer_POA)
+    CORBA_ORB_resolve_initial_references(orb, "RootPOA", ev);
   if (ev->_major != CORBA_NO_EXCEPTION) {
-    fprintf(stderr, "Error: could not get RootPOA: %s\n", 
-	    CORBA_exception_id(ev));
+    fprintf(stderr, "Error: could not get RootPOA: %s\n", CORBA_exception_id(ev));
     exit(1);
   }
 
@@ -46,21 +44,28 @@ int main(int argc, char* argv[]) {
   pm = PortableServer_POA__get_the_POAManager(root_poa, ev);
   PortableServer_POAManager_activate(pm, ev);
 
-  objref = CORBA_ORB_object_to_string(orb, dcf_text, ev);
-  fprintf(stderr, "%s\n", objref);
+  ior = CORBA_ORB_object_to_string(orb, dcf_text, ev);
+  fprintf(stderr, "%s\n", ior);
+  CORBA_free(ior);
 
-  /* So I have to use goad ???
-  name_server = gnome_name_service_get();
-  CosNaming_NamingContext_bind(name_server, &name, dcf_text, ev);
-  if (ev->_major != CORBA_NO_EXCEPTION) {
-    fprintf(stderr, "Error: could register object: %s\n",
-	    CORBA_exception_id(ev));
+  name_service = gnome_name_service_get();
+  switch (goad_server_register(CORBA_OBJECT_NIL, dcf_text, "dcf-text", "server", ev)) {
+  case -2:
+    /* There is a dcf-text running; bring it forward and quit */
+    dcf_text = goad_server_activate_with_repo_id(NULL, "IDL:GNOME/dcf-text:1.0",
+						 0, NULL);
+    CORBA_exception_free (ev);
+    exit(0);
+  case -1:
+    g_warning("There was trouble with the name server.\n"
+	      "aborting...\n");
     exit(1);
+  default:
+    g_warning("Beautiful, isn't it?\n");
   }
-  */
   
   CORBA_ORB_run(orb, ev);
 
-  CORBA_exception_free(ev);
+  CORBA_exception_free (ev);
   return 0;
 }
