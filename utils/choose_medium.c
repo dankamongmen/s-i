9e6f69c0ef2aa447342b0a8822c34f2f0b8de140
dwhedon <dwhedon@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2001-07-19 04:12:14 +0000

    start of choose_medium for di, nothing really here yet
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@581 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ utils/choose_medium.c
@@ -0,0 +1,17 @@
+/*
+  file - choose_medium.c
+  author - David Whedon <dwhedon@debian.org>
+  purpose - provide a framework for the debian-installer to choose
+  disks, nfs hosts, etc. when the installer needs to obtain or
+  write data.
+*/
+
+#include "choose_medium.h"
+
+int
+choose_medium(struct media *media) {
+
+
+
+
+}
