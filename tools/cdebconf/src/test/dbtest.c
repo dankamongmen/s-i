f8ee9c56a07b04cc6d850b845cf578220449db46
tfheen <tfheen@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2002-08-02 18:03:33 +0000

    extend test cases make test compile again
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@787 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/cdebconf/src/test/dbtest.c
+++ tools/cdebconf/src/test/dbtest.c
@@ -2,11 +2,44 @@
 #include "configuration.h"
 #include "frontend.h"
 #include "database.h"
+#include "template.h"
+#include "question.h"
+
+void loadtemplate(const char *filename, const char *owner, 
+                  struct template_db *g_templates,
+                  struct question_db *g_questions);
+
+void loadtemplate(const char *filename, const char *owner, 
+                  struct template_db *g_templates,
+                  struct question_db *g_questions) {
+	struct template *t;
+	struct question *q;
+
+	t = template_load(filename);
+	while (t)
+	{
+		if (g_templates->methods.set(g_templates, t) != DC_OK)
+			INFO(INFO_ERROR, "Cannot add template %s", t->tag);
+
+		q = g_questions->methods.get(g_questions, t->tag);
+		if (q == NULL)
+		{
+			q = question_new(t->tag);
+			q->template = t;
+		}
+		question_owner_add(q, owner);
+		if (g_questions->methods.set(g_questions, q) != DC_OK)
+			INFO(INFO_ERROR, "Cannot add question %s", t->tag);
+		question_deref(q);
+		t = t->next;
+	}
+}
 
 int main(int argc, char **argv)
 {
 	struct configuration *config;
-	struct database *db;
+	struct question_db *qdb;
+	struct template_db *tdb;
 
 	/* parse the configuration info */
 	config = config_new();
@@ -14,15 +47,24 @@ int main(int argc, char **argv)
 		DIE("Error reading configuration information");
 
 	/* initialize database and frontend modules */
-	if ((db = database_new(config)) == 0)
-		DIE("Cannot initialize DebConf database");
+	if ((tdb = template_db_new(config)) == 0)
+		DIE("Cannot initialize DebConf template database");
+
+	if ((qdb = question_db_new(config, tdb)) == 0)
+		DIE("Cannot initialize DebConf question database");
 
 	/* load templates */
-	db->load(db);
+	qdb->methods.load(qdb);
+	tdb->methods.load(tdb);
+
+        loadtemplate("test.templates","foo", tdb, qdb);
 
 	/* shutting down .... sync the database and shutdown the modules */
-	db->save(db);
-	database_delete(db);
+	qdb->methods.save(qdb);
+	tdb->methods.save(tdb);
+
+	template_db_delete(tdb);
+	question_db_delete(qdb);
 
 	return 0;
 }
