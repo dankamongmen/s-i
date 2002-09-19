fdee5c8006676b54ccefc2d011533eedc3c312ad
sjogren <sjogren@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2002-09-19 20:59:32 +0000

    Split out the code for finding retrievers into a helper function
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@994 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- anna/retriever.c
+++ anna/retriever.c
@@ -13,56 +13,66 @@
 #include <unistd.h>
 #include "anna.h"
 
+/* helper function for chosen_retriever() */
+static char *
+get_retriever_names()
+{
+	DIR *retrdir;
+	struct dirent *d_ent;
+	int retstr_size = 1, fname_len;
+	char *ret_choices;
+	char *fname;
+	struct stat st;
+
+	/* Find out which retrievers are available */
+	retrdir = opendir(RETRIEVER_DIR);
+	ret_choices = malloc(1);
+	ret_choices[0] = '\0';
+	while ((d_ent = readdir(retrdir)) != NULL) {
+		fname_len = strlen(d_ent->d_name);
+		fname = (char *)malloc(strlen(RETRIEVER_DIR) + 1 + 
+				fname_len);
+		strcpy(fname, RETRIEVER_DIR "/");
+		strcat(fname, d_ent->d_name);
+		stat(fname, &st);
+		free(fname);
+		if (S_ISREG(st.st_mode)) {
+			/* Should we check for x flag too? */
+			retstr_size += fname_len + 2;
+			ret_choices = realloc(ret_choices, retstr_size);
+			strcat(ret_choices, d_ent->d_name);
+			strcat(ret_choices, ", ");
+		}
+	}
+	closedir(retrdir);
+	if (retstr_size >= 3)
+		ret_choices[retstr_size-3] = '\0';
+	return ret_choices;
+}
+
 /* Returns the filename of the retriever to use. */
 /* TODO: error handling */
 char *chosen_retriever (void) {
 	static char *retriever = NULL;
-        
-	if (retriever == NULL) {
-                DIR *retrdir;
-		struct dirent *d_ent;
-		struct debconfclient *debconf;
-		int retstr_size = 1, fname_len;
-		char *ret_choices, *ret_default = NULL;
-		char *fname;
-                struct stat st;
 
-		/* Find out which retrievers are available */
-		retrdir = opendir(RETRIEVER_DIR);
-		ret_choices = malloc(1);
-                ret_choices[0] = '\0';
-		while ((d_ent = readdir(retrdir)) != NULL) {
-			fname_len = strlen(d_ent->d_name);
-			fname = (char *)malloc(strlen(RETRIEVER_DIR) + 1 + 
-                                                      fname_len);
-			strcpy(fname, RETRIEVER_DIR "/");
-			strcat(fname, d_ent->d_name);
-			stat(fname, &st);
-			free(fname);
-			if (S_ISREG(st.st_mode)) {
-				/* Should we check for x flag too? */
-				retstr_size += fname_len + 2;
-                                ret_choices = realloc(ret_choices, retstr_size);
-				strcat(ret_choices, d_ent->d_name);
-				strcat(ret_choices, ", ");
-				/* Pick the first one to be default :) */
-				if (ret_default == NULL)
-					ret_default = strdup(d_ent->d_name);
-			}
-                }
-		closedir(retrdir);
-		if (retstr_size >= 3)
-			ret_choices[retstr_size-3] = '\0';
+	if (retriever == NULL) {
+		struct debconfclient *debconf;
+		char *ret_choices, *ret_default;
+
+		ret_choices = get_retriever_names();
+		ret_default = strrchr(ret_choices, ',');
+		if (ret_default != NULL)
+			ret_default += 2;
 
 		debconf = debconfclient_new();
 		debconf->command(debconf, "TITLE", "Choose Retriever", NULL);
 		debconf->command(debconf, "SET", ANNA_RETRIEVER, ret_default, 
-                                 NULL);
+				NULL);
 		debconf->command(debconf, "SUBST", ANNA_RETRIEVER, "RETRIEVER", 
-                                 ret_choices, NULL);
+				ret_choices, NULL);
 		debconf->command(debconf, "SUBST", ANNA_RETRIEVER, "DEFAULT", 
-                                 ret_default, NULL);
-                debconf->command(debconf, "INPUT medium", ANNA_RETRIEVER, NULL);
+				ret_default, NULL);
+		debconf->command(debconf, "INPUT medium", ANNA_RETRIEVER, NULL);
 		debconf->command(debconf, "GO", NULL);
 		debconf->command(debconf, "GET", ANNA_RETRIEVER, NULL);
 		retriever = malloc(strlen(RETRIEVER_DIR) + 1 + 
@@ -72,7 +82,6 @@ char *chosen_retriever (void) {
 		debconfclient_delete(debconf);
                 
 		free(ret_choices);
-		free(ret_default);
         }
         return retriever;
 }
