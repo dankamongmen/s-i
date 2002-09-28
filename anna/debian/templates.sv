6476e4e62ab2ece5430de3500cb2cc86f169db73
sjogren <sjogren@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2002-09-28 07:59:29 +0000

    Added Swedish translation of anna's templates
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@1016 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ anna/debian/templates.sv
@@ -0,0 +1,11 @@
+Template: anna/retriever
+Type: select
+Choices: ${CHOICES}
+Choices-sv: ${CHOICES}
+Default: ${DEFAULT}
+Default-sv: ${DEFAULT}
+Description: Choose the retriever to use.
+ The retriever is responsible for fetching the packages to be
+ installed.
+Description-sv: Välj vilken hämtare (retriever) som ska användas.
+ Hämtaren ansvarar för att hämta paketen som ska installeras.
