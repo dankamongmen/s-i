59dbf5f079c44b7aa2984bade960371ff6f9408d
barbier <barbier@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2002-10-29 23:29:09 +0000

    * Convert to po-debconf, set Build-Depends: debhelper (>= 4.1.13)     to ensure that generated templates are right, and set output encoding     to UTF-8.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@1150 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- main-menu/debian/templates.sv
+++
@@ -1,9 +0,0 @@
-Template: debian-installer/main-menu
-Type: select
-Choices: ${MENU}
-Choices-sv: ${MENU}
-Default: ${DEFAULT}
-Description: Choose the next step:
- Here is the main menu of the Debian installer.
-Description-sv: Välj nästa steg:
- Det här är huvudmenyn för Debians installationsprogram.
