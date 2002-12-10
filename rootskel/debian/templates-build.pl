e1c735afa79cb270246ce2d3f29bfb09e012b676
waldi <waldi@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2002-12-10 20:09:35 +0000

    - rewrite template building, supports defaults values. - add initrd option.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@1530 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ rootskel/debian/templates-build.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my %template;
+$template{Fields} = [];
+$template{'Description-Long'} = "";
+
+while ( <STDIN> ) {
+	chomp;
+	if (m/^$/) {
+		foreach ( @{$template{Fields}} ) {
+			print $_ . ": ";
+			if ( ref $template{$_} eq "HASH" ) {
+				if ( defined $template{$_}->{$ARGV[0]} ) {
+					print $template{$_}->{$ARGV[0]};
+				} else {
+					print $template{$_}->{default};
+				}
+			} else {
+				print $template{$_};
+			}
+			print "\n";
+		}
+		print $template{'Description-Long'} . "\n";
+
+		%template = ();
+		$template{Fields} = [];
+		$template{'Description-Long'} = "";
+	} elsif ( m/^(\w+)(\[(\w+)\])?:\s+(.*)\s*$/ ) {
+		if ( defined $3 ) {
+			if ( defined $template{$1} and ref $template{$1} ne "HASH" ) {
+				local $_;
+				$_ = $template{$1};
+				$template{$1} = ();
+				$template{$1}->{default} = $_;
+			} elsif ( not defined $template{$1} ) {
+				push ( @{$template{Fields}}, $1 );
+			}
+			$template{$1}->{$3} = $4;
+		} else {
+			$template{$1} = $4;
+			push ( @{$template{Fields}}, $1 );
+		}
+	} else {
+		$template{'Description-Long'} .= $_ . "\n";
+	}
+}
+
