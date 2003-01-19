19cef4957bc75b6d07c1d672c5cde3a0e8cd7363
mckinstry <mckinstry@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-01-19 12:21:10 +0000

    move #defines here
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@1775 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ tools/kbd-chooser/config.h
@@ -0,0 +1,15 @@
+/* config.h
+ * 
+ */
+
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define DATADIR "/usr/share"
+#define KEYMAPDIR "keymaps"
+#define DEFMAP "us"
+
+#define PROGNAME "kbd-chooser"
+
+#endif
