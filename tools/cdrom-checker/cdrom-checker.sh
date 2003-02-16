22f9725399238a74e24970467725a171bee69c74
tsauter <tsauter@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-02-16 02:37:20 +0000

    Support now checking of several cdroms.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@1957 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/cdrom-checker/cdrom-checker.sh
+++ tools/cdrom-checker/cdrom-checker.sh
@@ -3,11 +3,37 @@
 . /usr/share/debconf/confmodule
 
 TARGET="/cdrom"
+DEVICE=`mount | grep ' /cdrom ' | cut -d " " -f 1`
 TMPFILE="/tmp/md5tmp"
+
+ask_mount() {
+	db_fset cdrom-checker/askmount seen false
+	db_input high cdrom-checker/askmount
+	db_go
+
+	mount -t auto $DEVICE $TARGET -o ro
+	if [ $? -ne 0 ]; then
+		db_fset cdrom-checker/mntfailed seen false
+		db_input high cdrom-checker/mntfailed
+		db_go
+		exit 0
+	fi
+}
+
+cd_check() {
+	if [ ! -d ".disk" -o ! -f "md5sum.txt" -o ! -d "debian" ]; then
+		db_fset cdrom-checker/wrongcd seen false
+		db_input high cdrom-checker/wrongcd
+		db_go
+		exit 0
+	fi
+}
 
 cleanup() {
 	[ -f "$TMPFILE" ] && rm -f $TMPFILE
 }
+
+
 
 # ask the user if we should check
 db_set cdrom-checker/start "false"
@@ -17,60 +43,71 @@ db_go
 db_get cdrom-checker/start
 [ "$RET" != "true" ] && exit 0
 
-# check the cdrom, and exit on error
-if [ ! -d "${TARGET}/.disk" -o ! -f "${TARGET}/md5sum.txt" ]; then
-	db_fset cdrom-checker/wrongcd seen false
-	db_input high cdrom-checker/wrongcd
-	db_go
-	exit 1
-fi
+# goeing here into main-loop
+while [ 1 ]; do
+	# make sure, cdrom is already mounted under $TARGET
+	grep -q " $TARGET " /proc/mounts
+	[ $? -ne 0 ] && ask_mount
 
-cd $TARGET
+	# cdrom is mounted, start checking
+	cd $TARGET
+	cd_check
+	#db_progress_start 0 $mcount cdrom-checker/progress_title	# FIXME
 
-# calculate the max percent
-set -- `wc -l md5sum.txt`
-mcount="$1"
+	# calculate the max percent
+	set -- `wc -l md5sum.txt`
+	mcount="$1"
 
-# check each file seperate, we can use a process bar for this
-#db_progress_start 0 $mcount cdrom-checker/progress_title
-
-cleanup
-count=0
-cat md5sum.txt 2>/dev/null | \
-while read LINE; do
-	count=$(($count+1))
-	set -- $LINE
-	file=`echo $2 | sed -e 's/^\.\///'`
-	# TODO:
-	#db_subst cdrom-checker/progress_step FILE "$file"
-	#db_progress_step 1 cdrom-checker/progress_step
-	echo "$1  $2" >$TMPFILE
-	md5sum -c $TMPFILE 1>/dev/null 2>&1
-	if [ $? -ne 0 ]; then
-		echo -n "$2" >$TMPFILE
-		break
-	fi
 	cleanup
+	count=0
+	cat md5sum.txt 2>/dev/null | \
+	while read LINE; do
+		count=$(($count+1))
+		set -- $LINE
+		file=`echo $2 | sed -e 's/^\.\///'`
+		#db_subst cdrom-checker/progress_step FILE "$file"
+		#db_progress_step 1 cdrom-checker/progress_step
+		echo "$1  $2" >$TMPFILE
+		md5sum -c $TMPFILE 1>/dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			echo -n "$2" >$TMPFILE
+			break
+		fi
+		cleanup
+	done
+
+	cd /	# make sure, cdrom is not bussy
+	umount /cdrom
+	#db_progress_stop			# FIXME
+
+	# some file is faulty, stop here
+	if [ -f "$TMPFILE" ]; then
+		file=`cat $TMPFILE | sed -e 's/^\.\///'`
+		db_subst cdrom-checker/missmatch FILE "$file"
+		db_fset cdrom-checker/missmatch seen false
+		db_input critical cdrom-checker/missmatch
+		db_go
+		cleanup
+		exit 0
+	fi
+
+	# notice the user about success
+	db_fset cdrom-checker/passed seen false
+	db_input high cdrom-checker/passed
+	db_go
+
+	# ask for next cdrom
+	db_set cdrom-checker/nextcd "false"
+	db_fset cdrom-checker/nextcd seen false
+	db_input high cdrom-checker/nextcd
+	db_go
+	db_get cdrom-checker/nextcd
+	[ "$RET" != "true" ] && break
 done
 
-# stop the progress bar
-#db_progress_stop
-
-# some file is faulty, stop here
-if [ -f "$TMPFILE" ]; then
-	file=`cat $TMPFILE | sed -e 's/^\.\///'`
-	db_subst cdrom-checker/missmatch FILE "$file"
-	db_fset cdrom-checker/missmatch seen false
-	db_input critical cdrom-checker/missmatch
-	db_go
-	cleanup
-	echo "failed"
-	exit 1
-fi
-
-# notice the user about success
-db_fset cdrom-checker/passed seen false
-db_input high cdrom-checker/passed
+db_fset cdrom-checker/firstcd seen false
+db_input high cdrom-checker/firstcd
 db_go
+mount -t auto $DEVICE $TARGET -o ro
 exit 0
 
