7dcc9be93c0757f82542a5fb740d1ca644db1655
pere <pere@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-03-06 11:02:59 +0000

    Correct prebaseconfig/40fstab.  It should now insert the file system mount points as well.  Device paths are rewritten using mapdevfs. Add mount points /floppy and /cdrom.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@2165 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- utils/fstab.sh
+++ utils/fstab.sh
@@ -6,29 +6,38 @@ FSTAB="$TARGET/etc/fstab"
 # make sure /etc exists
 mkdir -p `dirname $FSTAB`
 
-# print fstab header
-echo "# /etc/fstab: static file system information.">$FSTAB
-echo "#">>$FSTAB
-echo "# <file system> <mount point>   <type>  <options>               <dump>  <pass>">>$FSTAB
+(
+	# print fstab header
+    	echo "# /etc/fstab: static file system information."
+	echo "#"
+        echo "# <file system>	<mount point>	<type>	<options>	<dump>	<pass>"
+	# print filesystems
+	grep " $TARGET" /proc/mounts | \
+		while read LINE; do
+		set -- `echo $LINE`
+		mnt=`echo $2 | sed -e "s#^$TARGET##"`
+		[ -z "$mnt" ] && mnt="/"
+		pass="2"
+		[ "$mnt" = "/" ] && pass="1"
+		devpath=`mapdevfs $1` || devpath=$1
+		echo "$devpath	$mnt	$5	defaults	0	$pass"
+	done
+	
+	# print swaps
+	for i in `cat /proc/swaps | grep '^/' | cut -d " " -f 1`; do
+		devpath=`mapdevfs $i` || devpath=$i
+		echo "$devpath	none	swap	sw		0	0"
+	done
 
-# print filesystems
-grep "on $TARGET" /proc/mounts | \
-while read LINE; do
-	set -- `echo $LINE`
-	mnt=`echo $3 | sed -e "s#^$TARGET##"`
-	[ -z "$mnt" ] && mnt="/"
-	pass="2"
-	[ "$mnt" = "/" ] && pass="1"
-	echo "$1\t$mnt\t$5\tdefaults\t0\t$pass">>$FSTAB
-done
+	# print other generic mounts
+	echo "proc		/proc	proc	defaults	0	0"
 
-# print swaps
-for i in `cat /proc/swaps | grep '^/' | cut -d " " -f 1`; do
-	echo "$i\t/none\tswap\tsw\t0\t0">>$FSTAB
-done
+	# Print floppy
+	devpath=`mapdevfs /dev/floppy/0` || devpath=/dev/fd0
+	echo "$devpath	/floppy	auto	rw,user,noauto	0	0"
 
-# print other generic mounts
-echo "proc\t/proc\tproc\tdefaults\t0\t0">>$FSTAB
+	# Print CDROM
+	echo "/dev/cdrom	/cdrom	iso9660	ro,user,noauto	0	0"
+) > $FSTAB
 
 exit 0
-
