d6aa2618211dd5fa9855552a98c3598ae2f22ca6
sley <sley@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-03-26 14:03:29 +0000

    Send debconf a CAPB with no capabilities to disable backing-up. This is done each time the main-menu is displayed.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@2531 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- main-menu/main-menu.c
+++ main-menu/main-menu.c
@@ -185,6 +185,7 @@ struct package_t *show_main_menu(struct linkedlist_t *list) {
 
 	/* Make debconf show the menu and get the user's choice. */
         debconf->command(debconf, "TITLE", "Debian Installer Main Menu", NULL);
+	debconf->command(debconf, "CAPB", NULL);
 	debconf->command(debconf, "FSET", MAIN_MENU, "seen", "false", NULL);
 	debconf->command(debconf, "SUBST", MAIN_MENU, "MENU", menutext, NULL);
 	if (menudefault)
