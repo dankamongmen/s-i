d65d8232c5a6a18117fad8261b88caf98eea9658
pere <pere@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-04-18 21:17:58 +0000

    Started on shell replacement for ethdetect.c.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@2777 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ tools/ddetect/ethdetect.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+set -e
+. /usr/share/debconf/confmodule
+#set -x
+
+is_not_loaded() {
+    module="$1"
+    if cut -d" " -f1 /proc/modules | grep -q "$module" ; then
+	false
+    else
+	true
+    fi
+}
+
+module_insmod() {
+    module="$1"
+    db_input low ethdetect/module_params || [ $? -eq 30 ]
+    db_go
+    db_get ethdetect/module_params
+    if modprobe -v "$module" $RET ; then
+	prebaseconfig=/usr/lib/prebaseconfig.d/40ethdetect
+	echo "echo \"$module $RET\" >> /target/etc/modules" >> $prebaseconfig
+    else
+	db_input critical ethdetect/error || [ $? -eq 30 ]
+	db_go
+    fi
+}
+
+db_title "Network Hardware Configuration"
+
+db_input low ethdetect/detection_type || [ $? -eq 30 ]
+db_go
+
+db_get ethdetect/detection_type
+if [ true = "$RET" ] ; then
+    # Autodetect using hwdetect from disk-detect or cdrom-detect
+    hwdetect || true
+fi
+
+if [ -z "`sed -e "s/lo://" < /proc/net/dev | grep "[a-z0-9]*:[ ]*[0-9]*"`"  ]
+then
+    # No ethernet card.  Try manual loading
+    db_input high ethdetect/module_select || [ $? -eq 30 ]
+    db_go
+
+    db_get ethdetect/module_select
+    if [ other = "$RET" ] ; then
+	db_input high ethdetect/module_prompt || [ $? -eq 30 ]
+	db_go
+	db_get ethdetect/module_prompt
+    fi
+    module="$RET"
+    if is_not_loaded "$module" ; then
+	module_insmod "$module"
+    fi
+fi
