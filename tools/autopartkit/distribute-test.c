aa1b02572236203d4b5ce57ecdede1e7e9716d15
pere <pere@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-06-07 10:28:38 +0000

    Avoid duplicate implementation of autopartkit_error() and autopartkit_log().
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@3298 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/autopartkit/distribute-test.c
+++ tools/autopartkit/distribute-test.c
@@ -42,31 +42,6 @@ struct disk_info_t diskinfo[] = {
     { NULL, EMPTYGEOMETRY, 0, 0 }
 };
 
-void
-autopartkit_error(int fatal, const char * format, ...)
-{
-    va_list ap;
-
-    va_start(ap, format);
-    fprintf(stderr, "error: ");
-    vfprintf(stderr, format, ap);
-    va_end(ap);
-    if (fatal)
-      exit(fatal);
-}
-
-void
-autopartkit_log(const int level, const char * format, ...)
-{
-    va_list ap;
-
-    fprintf(stderr, "log %d: ", level);
-
-    va_start(ap, format);
-    vfprintf(stderr, format, ap);
-    va_end(ap);
-}
-
 int
 main(int argc, char *argv[])
 {
--- tools/autopartkit/dumptable.c
+++ tools/autopartkit/dumptable.c
@@ -1,31 +1,6 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "autopartkit.h"
-
-void
-autopartkit_error(int fatal, const char * format, ...)
-{
-    va_list ap;
-
-    va_start(ap, format);
-    fprintf(stderr, "error: ");
-    vfprintf(stderr, format, ap);
-    va_end(ap);
-    if (fatal)
-      exit(fatal);
-}
-
-void autopartkit_log(const int level, const char * format, ...)
-{
-    va_list ap;
-
-    fprintf(stderr, "log %d: ", level);
-
-    va_start(ap, format);
-    vfprintf(stderr, format, ap);
-    va_end(ap);
-}
 
 void
 list_dump(diskspace_req_t *list)
