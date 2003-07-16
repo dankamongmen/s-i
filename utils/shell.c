62f4a4fce78f8eaac2ce70bee0277386973691a0
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-07-16 16:38:21 +0000

    dup stderr too, since main-menu eats it
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@3555 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- utils/shell.c
+++ utils/shell.c
@@ -21,7 +21,8 @@ main ()
   client->command (client, "go", NULL);
 
   if ((dup2 (DEBCONF_OLD_STDIN_FD, 0) == -1)
-      || (dup2 (DEBCONF_OLD_STDOUT_FD, 1) == -1))
+      || (dup2 (DEBCONF_OLD_STDOUT_FD, 1) == -1)
+      || (dup2 (DEBCONF_OLD_STDOUT_FD, 2) == -1))
     {
       return -1;
     }
