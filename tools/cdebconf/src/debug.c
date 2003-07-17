0bbb62432e9c029fd3578bde092a78cb1b3aa10a
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-07-17 07:53:06 +0000

    added syslog logging for udeb
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@3564 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/cdebconf/src/debug.c
+++ tools/cdebconf/src/debug.c
@@ -2,12 +2,16 @@
 #include "common.h"
 #include <stdio.h>
 #include <stdarg.h>
+#ifdef SYSLOG_LOGGING
+#include <syslog.h>
+#endif
 
 void debug_printf(int level, char *fmt, ...)
 {
 	static int loglevel = -1;
 	static FILE *fp = NULL;
 	va_list ap;
+
 	if (loglevel < 0)
 	{
 		loglevel = (getenv("DEBCONF_DEBUG") 
@@ -20,11 +24,17 @@ void debug_printf(int level, char *fmt, ...)
 
 	if (level <= loglevel)
 	{
+#ifndef SYSLOG_LOGGING
 		va_start(ap, fmt);
 		vfprintf(fp, fmt, ap);
 		va_end(ap);
 
 		fflush(fp);
+#else
+		va_start(ap, fmt);
+		vsyslog(LOG_USER | LOG_DEBUG, fmt, ap);
+		va_end(ap);
+#endif
 	}
 }
-#endif
+#endif /* DEBUG */
