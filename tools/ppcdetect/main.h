9bd8e395eb8c96bcfc4f8b03b64f41ceaa8bd159
tsauter <tsauter@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-09-27 09:37:44 +0000

    insert function prototypes
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@4232 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/ppcdetect/main.h
+++ tools/ppcdetect/main.h
@@ -6,6 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0
+
+
+static void detect_newworld();
+static void detect_prep();
+static void detect_chrp();
+
+int check_value(const char *key, const char* value, const char *msg);
 int main(int argc, char *argv[]);
 
 #endif /* __MAIN_H__ */
