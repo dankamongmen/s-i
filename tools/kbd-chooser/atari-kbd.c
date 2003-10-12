51ae2d3d718438791e694cfb5439c1f364ad9d61
mckinstry <mckinstry@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-10-12 19:56:23 +0000

    Fix bad cut-n-paste from amiga code.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@4741 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/kbd-chooser/atari-kbd.c
+++ tools/kbd-chooser/atari-kbd.c
@@ -5,7 +5,7 @@
  * Copyright (C) 2003 Alastair McKinstry, <mckinstry@debian.org>
  * Released under the GPL
  *
- * $Id: atari-kbd.c,v 1.1 2003/10/05 07:15:16 mckinstry Exp $
+ * $Id: atari-kbd.c,v 1.2 2003/10/12 19:56:23 mckinstry Exp $
  */
 
 #include "config.h"
@@ -19,16 +19,13 @@
 /**
  * @brief list of keyboards present
  */
-kbd_t *amiga_kbd_get (kbd_t *keyboards)
+kbd_t *atari_kbd_get (kbd_t *keyboards)
 {
 	kbd_t *k = xmalloc (sizeof (kbd_t));
 
-	// /proc must be mounted by this point
-	assert (di_check_dir ("/proc") == 1);
-
-	k->name = "amiga"; // This must match the name "amiga" in console-keymaps-amiga
-	k->description = _("Amiga Keyboard");
-	k->deflt = "amiga-us";
+	k->name = "atari"; // This must match the name "amiga" in console-keymaps-amiga
+	k->description = _("Atari Keyboard");
+	k->deflt = "atari-us";
 	k->fd = -1;
 	k->present = UNKNOWN;
 	k->next = keyboards;
@@ -36,19 +33,7 @@ kbd_t *amiga_kbd_get (kbd_t *keyboards)
 
 		
 #if defined (KERNEL_2_6)
-	// In 2.6 series, we can detect keyboard via /proc/bus/input
-	if (di_check_dir ("/proc/bus/input") >= 0) {
-		int res;
-		// this dir only present in 2.6
-		res = grep ("/proc/bus/input/devices", "Amiga keyboard");
-		if (res < 0) {
-			di_warning ("amiga-kbd: Failed to open /proc/bus/input/devicves");
-			return keyboards;
-		}
-		k->present = ( res == 0) ? TRUE : FALSE;
-		return keyboards;
-	}
-
+	// FIXME Write this
 
 #endif // KERNEL_2_6
 
