e80a883d6a0d7453e17975e137546c6e32cd2f34
mckinstry <mckinstry@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-10-14 20:42:02 +0000

    - Only m68, powerpc, only offer amiga keyboard on amiga machines.  - Remove Description field from kbd struct; now pulled in from debconf (for i18n),  - Move /proc check into kernel-2.6 specific code
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@4875 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/kbd-chooser/amiga-kbd.c
+++ tools/kbd-chooser/amiga-kbd.c
@@ -5,30 +5,34 @@
  * Copyright (C) 2003 Alastair McKinstry, <mckinstry@debian.org>
  * Released under the GPL
  *
- * $Id: amiga-kbd.c,v 1.6 2003/10/03 22:02:49 mckinstry Exp $
+ * $Id: amiga-kbd.c,v 1.7 2003/10/14 20:42:02 mckinstry Exp $
  */
 
 #include "config.h"
 #include <assert.h>
+#include <string.h>
 #include <debian-installer.h>
 #include "xmalloc.h"
-#include "nls.h"
 #include "kbd-chooser.h"
 
 
 /**
  * @brief list of keyboards present
  */
-kbd_t *amiga_kbd_get (kbd_t *keyboards)
+kbd_t *amiga_kbd_get (kbd_t *keyboards, const char *subarch)
 {
 	kbd_t *k = xmalloc (sizeof (kbd_t));
-	int res;
 
-	// /proc must be mounted by this point
-	assert (di_check_dir ("/proc") == 1);
+#if defined(__m68k__)
+	if (strstr (subarch, "amiga")== NULL)
+		return keyboards;
+#endif
+#if defined(__powerpc__)
+	if  (strstr (subarch, "apus") == NULL)
+		return keyboards;
+#endif
 
 	k->name = "amiga"; // This must match the name "amiga" in console-keymaps-amiga
-	k->description = _("Amiga Keyboard");
 	k->deflt = "amiga-us";
 	k->fd = -1;
 	k->present = UNKNOWN;
@@ -37,6 +41,10 @@ kbd_t *amiga_kbd_get (kbd_t *keyboards)
 
 		
 #if defined (KERNEL_2_6)
+
+	// /proc must be mounted by this point
+	assert (di_check_dir ("/proc") == 1);
+	
 	// In 2.6 series, we can detect keyboard via /proc/bus/input
 	if (di_check_dir ("/proc/bus/input") >= 0) {
 		int res;
