233061b413d61b3d003458a7dd6421b26af07f38
mckinstry <mckinstry@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-10-14 20:17:01 +0000

    - Move check for /proc into kernel-2.6 specific code  - Remove description field; now retrieved from template (for i18n)
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@4869 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/kbd-chooser/sparc-kbd.c
+++ tools/kbd-chooser/sparc-kbd.c
@@ -4,29 +4,24 @@
  * Copyright (C) 2003 Alastair McKinstry, <mckinstry@debian.org>
  * Released under the GPL
  *
- * $Id: sparc-kbd.c,v 1.6 2003/10/03 22:02:49 mckinstry Exp $
+ * $Id: sparc-kbd.c,v 1.7 2003/10/14 20:17:01 mckinstry Exp $
  */
 
 #include "config.h"
 #include <assert.h>
 #include <debian-installer.h>
 #include "xmalloc.h"
-#include "nls.h"
 #include "kbd-chooser.h"
 
 
 /**
  * @brief list of keyboards present
  */
-kbd_t *at_kbd_get (kbd_t *keyboards)
+kbd_t *sparc_kbd_get (kbd_t *keyboards, const char *subarch)
 {
 	kbd_t *k = xmalloc (sizeof(kbd_t));
 
-	// /proc must be mounted by this point
-	assert (di_check_dir ("/proc") == 1);
-
 	k->name = "sparc"; // This must match the name "sparc" in console-keymaps-sparc
-	k->description = _("Sun Keyboard");
 	k->deflt = "sunkeymap";
 	k->fd = -1;
 	k->present = UNKNOWN;
@@ -41,7 +36,11 @@ kbd_t *at_kbd_get (kbd_t *keyboards)
 	 * keymap (edit the keymaps list)
 	 */
 #warning "Kernel 2.6 code not written yet"
-	if (di_check_dir ("/proc/bus/input") >= 0) {
+
+	  // /proc must be mounted by this point
+	  assert (di_check_dir ("/proc") == 1); 
+	
+	  if (di_check_dir ("/proc/bus/input") >= 0) {
 		int res;
 		// this dir only present in 2.6
 		res = grep ("/proc/bus/input/devices","Sun Type");
