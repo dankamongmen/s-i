d46d4b263627e16694e73db99888c02eab9bc1f3
barbier <barbier@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-11-09 22:16:16 +0000

    Sort country names according to current language.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@5899 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- retriever/choose-mirror/makeheader.pl
+++ retriever/choose-mirror/makeheader.pl
@@ -58,7 +58,9 @@ if ($type eq 'template') {
 	}
 	print TEMPLATE "Template: mirror/http/countries\n";
         print TEMPLATE "Type: select\n";
-        print TEMPLATE "__Choices: enter information manually";
+        print TEMPLATE "Listorder: lexicographic\n";
+        print TEMPLATE "#  Translators, ignore leading exclamation mark,\n";
+        print TEMPLATE "__Choices: !enter information manually";
 	foreach  my $country (sort (keys %http_countries)) {
 		print TEMPLATE ", ${country}";
 	}
@@ -69,7 +71,9 @@ if ($type eq 'template') {
 
 	print TEMPLATE "Template: mirror/ftp/countries\n";
 	print TEMPLATE "Type: select\n";
-        print TEMPLATE "__Choices: enter information manually";
+        print TEMPLATE "Listorder: lexicographic\n";
+        print TEMPLATE "#  Translators, ignore leading exclamation mark,\n";
+        print TEMPLATE "__Choices: !enter information manually";
 	foreach  my $country (sort (keys %ftp_countries)) {
                 print TEMPLATE ", ${country}";
         }
