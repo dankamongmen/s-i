97496e9114f76da215836853187f14ab3fffd801
waldi <waldi@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2003-11-20 16:43:39 +0000

    - fix includes - don't build in maintainer mode
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@6302 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- libdebian-installer/include/debian-installer.h
+++ libdebian-installer/include/debian-installer.h
@@ -2,14 +2,15 @@
 
 #include <debian-installer/exec.h>
 #include <debian-installer/hash.h>
-//#include <debian-installer/list.h>
+#include <debian-installer/list.h>
 #include <debian-installer/log.h>
 #include <debian-installer/mem.h>
+#include <debian-installer/mem_chunk.h>
 #include <debian-installer/parser.h>
 #include <debian-installer/parser_rfc822.h>
 #include <debian-installer/package.h>
 #include <debian-installer/packages.h>
-//#include <debian-installer/release.h>
+#include <debian-installer/release.h>
 #include <debian-installer/slist.h>
 #include <debian-installer/string.h>
 #include <debian-installer/types.h>
