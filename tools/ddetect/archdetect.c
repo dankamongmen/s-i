041501333328265698721359149f4be69a86e693
waldi <waldi@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-01-20 12:44:50 +0000

    Add postinst script for archdetect.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@7961 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- tools/ddetect/archdetect.c
+++ tools/ddetect/archdetect.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	if (!(subarch = subarch_analyze()))
 		return 0;
 
-	if (strstr(argv[0], "postinst") != NULL) {
+	if (strstr(argv[0], "postinst") || (argc > 1 && strcmp(argv[1], "postinst") == 0)) {
                 struct debconfclient *debconf;
 		debconf = debconfclient_new();
 		debconf_set(debconf, "debian-installer/kernel/subarchitecture", subarch);
