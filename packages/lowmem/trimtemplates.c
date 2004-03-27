daf4ddcc1d33ee2494c3ffe8c82abe959ad0f782
sferriol-guest <sferriol-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-03-27 01:08:02 +0000

    modify trimtemplates.c to be compliant with the previous trimtempletes.sh
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@11985 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/lowmem/trimtemplates.c
+++ packages/lowmem/trimtemplates.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #define STATUS_FILE "/var/lib/lowmem"
 #define TEMPLATE_FILE_EXTENSION ".templates"
@@ -11,91 +12,120 @@
 #define CHOICES_TO_REMOVE "Choices-"
 #define FILENAME_LENGTH 256
 
-int main(int argc, char** argv) {
+int trimtemplate(char *filename) {
+     int ignore = 0;
      FILE *fd = NULL;
      FILE *tmpfd = NULL;
-     DIR  *dip = NULL;
-     struct dirent *dit = NULL;
      char template_line[TEMPLATE_LINE_MAX_LENGTH];
-     char *dir = NULL;
-     char abs_path_file_name[FILENAME_LENGTH];
-     char abs_path_tmpfile_name[FILENAME_LENGTH];
+     char tmpfilename[FILENAME_LENGTH];
 
-     int ignore = 0;
+     if ((fd = fopen(filename, "r")) == NULL) {
+	  perror("unable to open template file");
+	  return 0;
+     }
+     snprintf(tmpfilename,
+	      FILENAME_LENGTH,"%s.%s",
+	      filename,"tmp");
 
+     if ((tmpfd = fopen(tmpfilename, "w")) == NULL) {
+	  perror("unable to open temp file");
+	  return 0;
+     }
+
+     ignore = 0;
+	       
+     /* parse template file */
+     while (fgets(template_line, 
+		  TEMPLATE_LINE_MAX_LENGTH, fd) != NULL) {
+	  if (ignore && 
+	      strstr(template_line, " ") == template_line) {
+	       continue;
+	  }
+	  ignore = 0;
+	  if (strstr(template_line, DESCRIPTION_TO_REMOVE) 
+	      || strstr(template_line, CHOICES_TO_REMOVE)
+	      || strstr(template_line, DEFAULT_TO_REMOVE)
+	       ) {
+	       ignore = 1;
+	       continue;
+	  }
+	  if (fputs(template_line, tmpfd) == EOF) {
+	       perror("unable to write to temp file");
+	       return 0;
+	  }
+     }
+	       
+     fclose(tmpfd);
+     fclose(fd);
+
+     if( rename(tmpfilename, filename) != 0) { 
+	  perror("unable to rename temp to template file");
+	  return 0;
+     } 
+     return 1;
+}
+
+
+int main(int argc, char** argv) {
+     struct stat buf;
+     
      if (argc < 2) {
 	  return 0;
      }
 
-     dir = argv[1];
-
-     /* test if lowmem is activated */
-     if( rename(STATUS_FILE, STATUS_FILE) != 0) { 
-	  return 0;
-     } 
-  
-     if ((dip = opendir(dir)) == NULL) {
+     if ( stat(argv[1], &buf) == -1 ) {
+	  perror("unable to get info about specified file or directory");
 	  return 0;
      }
-     while ((dit = readdir(dip)) != NULL) {
-	  /* select only template files */
+     
+     if (S_ISDIR(buf.st_mode)) {
+	  DIR  *dip = NULL;
+	  struct dirent *dit = NULL;
+	  char *dir = NULL;
+	  char abs_path_file_name[FILENAME_LENGTH];
 
-	  if (strstr(dit->d_name, TEMPLATE_FILE_EXTENSION) != NULL) {
-	       
-	       snprintf(abs_path_file_name,
-			FILENAME_LENGTH,"%s/%s",dir,dit->d_name);
-	       
- 	       if ((fd = fopen(abs_path_file_name, "r")) == NULL) {
-		    perror("unable to open template file");
-		    return 0;
-	       }
-	       snprintf(abs_path_tmpfile_name,
-			FILENAME_LENGTH,"%s.%s",
-			abs_path_file_name,"tmp");
-
- 	       if ((tmpfd = fopen(abs_path_tmpfile_name, "w")) == NULL) {
-		    perror("unable to open temp file");
-		    return 0;
-	       }
-
-	       ignore = 0;
-	       
-	       /* parse template file */
-	       while (fgets(template_line, 
-			    TEMPLATE_LINE_MAX_LENGTH, fd) != NULL) {
-		    if (ignore && 
-			strstr(template_line, " ") == template_line) {
-			 continue;
-		    }
-		    ignore = 0;
-		    if (strstr(template_line, DESCRIPTION_TO_REMOVE) 
-			|| strstr(template_line, CHOICES_TO_REMOVE)
-			|| strstr(template_line, DEFAULT_TO_REMOVE)
-			 ) {
-			 ignore = 1;
-			 continue;
-		    }
-		    if (fputs(template_line, tmpfd) == EOF) {
-			 perror("unable to write to temp file");
+	  dir = argv[1];
+       
+	  /* test if lowmem is activated */
+	  if( rename(STATUS_FILE, STATUS_FILE) != 0) { 
+	       return 0;
+	  } 
+       
+	  if ((dip = opendir(dir)) == NULL) {
+	       return 0;
+	  }
+	  while ((dit = readdir(dip)) != NULL) {
+	       /* select only template files */
+	 
+	       if (strstr(dit->d_name, TEMPLATE_FILE_EXTENSION) != NULL) {
+	   
+		    snprintf(abs_path_file_name,
+			     FILENAME_LENGTH,"%s/%s",dir,dit->d_name);
+		    if (trimtemplate(abs_path_file_name) == 0) {
 			 return 0;
 		    }
 	       }
-	       
-	       fclose(tmpfd);
-	       fclose(fd);
-
-	       if( rename(abs_path_tmpfile_name, abs_path_file_name) != 0) { 
-			 perror("unable to rename temp to template file");
-		    return 0;
-	       } 
 	  }
+       
+	  if (closedir(dip) == -1) {
+	       return 0;
+	  }
+	  
+	  return 1;
      }
 
-     if (closedir(dip) == -1) {
-	  return 0;
+     if (S_ISREG(buf.st_mode)) {
+	  char *filename = NULL;
+
+	  filename = argv[1];
+
+	  if (trimtemplate(filename) == 0) {
+	       return 0;
+	  }
+	  return 1;
      }
-  
-     return 1;
+
+     return 0;
 }
 
 
