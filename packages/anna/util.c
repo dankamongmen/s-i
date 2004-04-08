2b040f9e9dd06a1cda6b9303e56774b06aa55d91
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-04-08 17:22:46 +0000

    have anna turn off backup before invoking error handlers
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@12915 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/anna/util.c
+++ packages/anna/util.c
@@ -127,12 +127,19 @@ signed int retriever_handle_error (const char *failing_command) {
     char *retriever, *command;
     int ret;
     
+    /* There is no mechanism to handle backing up from here, so turn it
+     * off. */
+    debconf_capb(debconf, "");
+    
     retriever = get_retriever();
     if (asprintf(&command, "%s error %s", retriever, failing_command) == -1)
         return 0;
     ret = di_exec_shell_log(command);
     free(retriever);
     free(command);
+    
+    debconf_capb(debconf, "backup");
+    
     switch (ret) {
         case 0:
             return 1;
