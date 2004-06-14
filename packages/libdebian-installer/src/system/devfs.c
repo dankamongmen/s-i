c35864e9c4f6a36c04c4a1fb8ff5894543bf9270
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-06-14 15:30:01 +0000

    * Joey Hess     - fix devfs.c test mode (to really work)     - fix /dev/ida support off by one     - fix /dev/ataraid support, leave off the cN part of device name     - don't map character devices to block device namesb
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@16805 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/libdebian-installer/src/system/devfs.c
+++ packages/libdebian-installer/src/system/devfs.c
@@ -26,6 +26,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -47,63 +48,65 @@ ssize_t di_system_devfs_map_from (const char *path, char *buf, size_t n)
     enum { ENTRY_TYPE_ONE, ENTRY_TYPE_NUMBER, ENTRY_TYPE_DISC, ENTRY_TYPE_DISC_ARRAY } type;
     unsigned char entry_first;
     unsigned char entry_disc_minor_shift;
+    enum { FORMAT_DEFAULT, FORMAT_NOCONTROLLER } format;
   }
   entries[] =
   {
     /*
-      major	minor	name		type			entry_first
+      bmajor	minor	name		type			entry_first
       									entry_disc_minor_shift
+										format
     */
-    { 2,	0,	"fd",		ENTRY_TYPE_NUMBER,	0,	0 },
-    { 3,	0,	"hd",		ENTRY_TYPE_DISC,	0,	6 },
-    { 4,	64,	"ttyS",		ENTRY_TYPE_NUMBER,	0,	0 },
-    { 4,	0,	"tty",		ENTRY_TYPE_NUMBER,	0,	0 },
-    { 8,	0,	"sd",		ENTRY_TYPE_DISC,	0,	4 },
-    { 9,	0,	"md",		ENTRY_TYPE_NUMBER,	0,	0 },
-    { 11,	0,	"scd",		ENTRY_TYPE_NUMBER,	0,	0 },
-    { 22,	0,	"hd",		ENTRY_TYPE_DISC,	2,	6 },
-    { 33,	0,	"hd",		ENTRY_TYPE_DISC,	4,	6 },
-    { 34,	0,	"hd",		ENTRY_TYPE_DISC,	6,	6 },
-    { 48,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	0,	3 },
-    { 49,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	1,	3 },
-    { 50,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	2,	3 },
-    { 51,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	3,	3 },
-    { 52,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	4,	3 },
-    { 53,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	5,	3 },
-    { 54,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	6,	3 },
-    { 55,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	7,	3 },
-    { 56,	0,	"hd",		ENTRY_TYPE_DISC,	8,	6 },
-    { 57,	0,	"hd",		ENTRY_TYPE_DISC,	10,	6 },
-    { 65,	0,	"sd",		ENTRY_TYPE_DISC,	16,	4 },
-    { 66,	0,	"sd",		ENTRY_TYPE_DISC,	32,	4 },
-    { 67,	0,	"sd",		ENTRY_TYPE_DISC,	48,	4 },
-    { 68,	0,	"sd",		ENTRY_TYPE_DISC,	64,	4 },
-    { 69,	0,	"sd",		ENTRY_TYPE_DISC,	80,	4 },
-    { 70,	0,	"sd",		ENTRY_TYPE_DISC,	96,	4 },
-    { 71,	0,	"sd",		ENTRY_TYPE_DISC,	112,	4 },
-    { 72,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	0,	3 },
-    { 73,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	1,	3 },
-    { 74,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	2,	3 },
-    { 75,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	3,	3 },
-    { 76,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	4,	3 },
-    { 77,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	5,	3 },
-    { 78,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	6,	3 },
-    { 79,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	7,	3 },
-    { 88,	0,	"hd",		ENTRY_TYPE_DISC,	12,	6 },
-    { 89,	0,	"hd",		ENTRY_TYPE_DISC,	14,	6 },
-    { 90,	0,	"hd",		ENTRY_TYPE_DISC,	16,	6 },
-    { 91,	0,	"hd",		ENTRY_TYPE_DISC,	18,	6 },
-    { 94,	0,	"dasd",		ENTRY_TYPE_DISC,	0,	2 },
-    { 104,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	0,	4 },
-    { 105,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	1,	4 },
-    { 106,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	2,	4 },
-    { 107,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	3,	4 },
-    { 108,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	4,	4 },
-    { 109,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	5,	4 },
-    { 110,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	6,	4 },
-    { 111,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	7,	4 },
-    { 114,	0,	"ataraid",	ENTRY_TYPE_DISC_ARRAY,	0,	4 },
-    { 0,	0,	NULL,		ENTRY_TYPE_ONE,		0,	0 },
+    { 2,	0,	"fd",		ENTRY_TYPE_NUMBER,	0,	0,	FORMAT_DEFAULT },
+    { 3,	0,	"hd",		ENTRY_TYPE_DISC,	0,	6,	FORMAT_DEFAULT },
+    { 4,	64,	"ttyS",		ENTRY_TYPE_NUMBER,	0,	0,	FORMAT_DEFAULT },
+    { 4,	0,	"tty",		ENTRY_TYPE_NUMBER,	0,	0,	FORMAT_DEFAULT },
+    { 8,	0,	"sd",		ENTRY_TYPE_DISC,	0,	4,	FORMAT_DEFAULT },
+    { 9,	0,	"md",		ENTRY_TYPE_NUMBER,	0,	0,	FORMAT_DEFAULT },
+    { 11,	0,	"scd",		ENTRY_TYPE_NUMBER,	0,	0,	FORMAT_DEFAULT },
+    { 22,	0,	"hd",		ENTRY_TYPE_DISC,	2,	6,	FORMAT_DEFAULT },
+    { 33,	0,	"hd",		ENTRY_TYPE_DISC,	4,	6,	FORMAT_DEFAULT },
+    { 34,	0,	"hd",		ENTRY_TYPE_DISC,	6,	6,	FORMAT_DEFAULT },
+    { 48,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	0,	3,	FORMAT_DEFAULT },
+    { 49,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	1,	3,	FORMAT_DEFAULT },
+    { 50,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	2,	3,	FORMAT_DEFAULT },
+    { 51,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	3,	3,	FORMAT_DEFAULT },
+    { 52,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	4,	3,	FORMAT_DEFAULT },
+    { 53,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	5,	3,	FORMAT_DEFAULT },
+    { 54,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	6,	3,	FORMAT_DEFAULT },
+    { 55,	0,	"rd",		ENTRY_TYPE_DISC_ARRAY,	7,	3,	FORMAT_DEFAULT },
+    { 56,	0,	"hd",		ENTRY_TYPE_DISC,	8,	6,	FORMAT_DEFAULT },
+    { 57,	0,	"hd",		ENTRY_TYPE_DISC,	10,	6,	FORMAT_DEFAULT },
+    { 65,	0,	"sd",		ENTRY_TYPE_DISC,	16,	4,	FORMAT_DEFAULT },
+    { 66,	0,	"sd",		ENTRY_TYPE_DISC,	32,	4,	FORMAT_DEFAULT },
+    { 67,	0,	"sd",		ENTRY_TYPE_DISC,	48,	4,	FORMAT_DEFAULT },
+    { 68,	0,	"sd",		ENTRY_TYPE_DISC,	64,	4,	FORMAT_DEFAULT },
+    { 69,	0,	"sd",		ENTRY_TYPE_DISC,	80,	4,	FORMAT_DEFAULT },
+    { 70,	0,	"sd",		ENTRY_TYPE_DISC,	96,	4,	FORMAT_DEFAULT },
+    { 71,	0,	"sd",		ENTRY_TYPE_DISC,	112,	4,	FORMAT_DEFAULT },
+    { 72,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	0,	4,	FORMAT_DEFAULT },
+    { 73,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	1,	4,	FORMAT_DEFAULT },
+    { 74,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	2,	4,	FORMAT_DEFAULT },
+    { 75,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	3,	4,	FORMAT_DEFAULT },
+    { 76,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	4,	4,	FORMAT_DEFAULT },
+    { 77,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	5,	4,	FORMAT_DEFAULT },
+    { 78,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	6,	4,	FORMAT_DEFAULT },
+    { 79,	0,	"ida",		ENTRY_TYPE_DISC_ARRAY,	7,	4,	FORMAT_DEFAULT },
+    { 88,	0,	"hd",		ENTRY_TYPE_DISC,	12,	6,	FORMAT_DEFAULT },
+    { 89,	0,	"hd",		ENTRY_TYPE_DISC,	14,	6,	FORMAT_DEFAULT },
+    { 90,	0,	"hd",		ENTRY_TYPE_DISC,	16,	6,	FORMAT_DEFAULT },
+    { 91,	0,	"hd",		ENTRY_TYPE_DISC,	18,	6,	FORMAT_DEFAULT },
+    { 94,	0,	"dasd",		ENTRY_TYPE_DISC,	0,	2,	FORMAT_DEFAULT },
+    { 104,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	0,	4,	FORMAT_DEFAULT },
+    { 105,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	1,	4,	FORMAT_DEFAULT },
+    { 106,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	2,	4,	FORMAT_DEFAULT },
+    { 107,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	3,	4,	FORMAT_DEFAULT },
+    { 108,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	4,	4,	FORMAT_DEFAULT },
+    { 109,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	5,	4,	FORMAT_DEFAULT },
+    { 110,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	6,	4,	FORMAT_DEFAULT },
+    { 111,	0,	"cciss",	ENTRY_TYPE_DISC_ARRAY,	7,	4,	FORMAT_DEFAULT },
+    { 114,	0,	"ataraid",	ENTRY_TYPE_DISC_ARRAY,	0,	4,	FORMAT_NOCONTROLLER },
+    { 0,	0,	NULL,		ENTRY_TYPE_ONE,		0,	0,	FORMAT_DEFAULT },
   };
 
   struct stat s;
@@ -129,12 +132,13 @@ ssize_t di_system_devfs_map_from (const char *path, char *buf, size_t n)
     }
     e++;
   }
-  if (!e->name)
+  if (!e->name || !S_ISBLK(s.st_mode)) {
 #ifdef TEST
     fprintf(stderr, "(unknown device)\n");
 #endif
     /* Pass unknown devices on without changes. */
     return snprintf (buf, n, "%s", path);
+  }
   
   strcat (buf, "/dev/");
 
@@ -173,7 +177,10 @@ ssize_t di_system_devfs_map_from (const char *path, char *buf, size_t n)
 
           break;
         case ENTRY_TYPE_DISC_ARRAY:
-          ret = di_snprintfcat (buf, n, "%s/c%dd%d", e->name, e->entry_first, disc);
+	  if (e->format == FORMAT_DEFAULT)
+	          ret = di_snprintfcat (buf, n, "%s/c%dd%d", e->name, e->entry_first, disc);
+	  else if (e->format == FORMAT_NOCONTROLLER)
+	          ret = di_snprintfcat (buf, n, "%s/d%d", e->name, disc);
 
           if (part)
             ret = di_snprintfcat (buf, n, "p%d", part);
@@ -184,7 +191,7 @@ ssize_t di_system_devfs_map_from (const char *path, char *buf, size_t n)
       }
       break;
   };
-
+  
   return ret;
 }
 
