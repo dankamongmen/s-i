6b5dac41bea2f648efcef4ca7339d94a66bfc43f
waldi <waldi@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-07-10 16:59:41 +0000

    Restore stderr.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@17697 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/debian-installer-utils/debian/di-utils-shell.postinst.c
+++ packages/debian-installer-utils/debian/di-utils-shell.postinst.c
@@ -1,5 +1,3 @@
-/* Yes, that's right, a compiled postinst. */
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -20,60 +18,61 @@ char *new_envp[] =
 };
 
 int main (int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)), char *envp[]) {
-	struct debconfclient *client;
-        char **t1, **t2;
-        pid_t pid;
+  struct debconfclient *client;
+  char **t1, **t2;
+  pid_t pid;
+  int i;
 
-	client = debconfclient_new();
+  client = debconfclient_new();
 
-	debconf_capb(client, "backup");
-	debconf_fset(client, "di-utils-shell/do-shell", "seen", "false");
-	debconf_input(client, "high", "di-utils-shell/do-shell");
-	if (debconf_go(client) == 30) {
-		exit(10);
-	}
+  debconf_capb(client, "backup");
+  debconf_fset(client, "di-utils-shell/do-shell", "seen", "false");
+  debconf_input(client, "high", "di-utils-shell/do-shell");
+  if (debconf_go(client) == 30)
+    exit(10);
 
-	/* 
-	 * To run the shell, we must first close the cdebconf file
-	 * descriptors. That's why this postinst is a C program in
-	 * the first place.
-	 */
-	if ((dup2(DEBCONF_OLD_STDIN_FD, 0) == -1) ||
-	    (dup2(DEBCONF_OLD_STDOUT_FD, 1) == -1))
-		exit(1);
+  /* 
+   * To run the shell, we must first close the cdebconf file
+   * descriptors. That's why this postinst is a C program in
+   * the first place.
+   */
+  for (i = 0; i <= 2; i++)
+    if (dup2(DEBCONF_OLD_FD_BASE + i, i) == -1)
+      exit(1);
 
-        system("clear");
+  system("clear");
 
-        t1 = new_envp;
-        while (*t1)
-        {
-          t2 = envp;
-          while (*t2)
-          {
-            if (strncmp (*t1, *t2, strlen (*t1)) == 0)
-            {
-              *t1 = *t2;
-              break;
-            }
-            t2++;
-          }
-          t1++;
-        }
+  t1 = new_envp;
+  while (*t1)
+  {
+    t2 = envp;
+    while (*t2)
+    {
+      if (strncmp (*t1, *t2, strlen (*t1)) == 0)
+      {
+        *t1 = *t2;
+        break;
+      }
+      t2++;
+    }
+    t1++;
+  }
 
-        pid = fork ();
+  pid = fork ();
 
-        if (pid == 0)
-        {
-          chdir("/");
+  if (pid == 0)
+  {
+    chdir("/");
 
-          execve ("/bin/sh", new_argv, new_envp);
-        }
-        else if (pid > 0)
-        {
-          int status;
-          if (!waitpid (pid, &status, 0))
-            return 0;
-        }
+    execve ("/bin/sh", new_argv, new_envp);
+  }
+  else if (pid > 0)
+  {
+    int status;
+    if (!waitpid (pid, &status, 0))
+      return 0;
+  }
 
-        return 0;
+  return 0;
 }
+
