8cd4207abede52f84f1d5012d7692cda78817095
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-09-13 18:51:36 +0000

    * Joey Hess     - Split out a library to avoid code duplication.     - Support includes via preseed/include.     - Allow multiple preseed files to be specified in pressed/url and       preseed/file.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@21561 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ packages/preseed/preseed.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+log () {
+	logger -t preseed "$@"
+}
+
+error () {
+	error="$1"
+	location="$2"
+	db_subst preseed/$error LOCATION "$location"
+	db_input critical preseed/$error || true
+	db_go || true
+	exit 1
+}
+
+# Note: Needs a preseed_fetch function or command not provided by this
+# file, as well as preseed_relative
+preseed_location () {
+	local location="$1"
+	
+	local tmp=/tmp/debconf-seed
+	local logfile=/var/log/debconf-seed
+	
+	if ! preseed_fetch $location $tmp; then
+		error retrieve_error "$location"
+	fi
+
+	db_set preseed/include ""
+	if ! debconf-set-selections $tmp; then
+		error load_error "$location"
+	fi
+		
+	if [ -e $logfile ]; then
+		cat $tmp >> $logfile
+	else
+		cp $tmp $logfile
+	fi
+	rm -f $tmp
+
+	log "successfully loaded $location"
+	local last_location="$location"
+	
+	db_get preseed/include
+	for location in $RET; do
+		# Support relative paths, just use path of last file.
+		if preseed_relative "$location"; then
+			# This works for urls too.
+			location="$(dirname $last_location)/$location"
+		fi
+		if [ -n "$location" ]; then
+			preseed_location "$location"
+		fi
+	done
+}
+	
+preseed () {
+	template="$1"
+
+	db_get $template
+	location="$RET"
+	if [ -n "$location" ]; then
+		for loc in $location; do
+			preseed_location "$loc"
+		done
+	fi
+}
