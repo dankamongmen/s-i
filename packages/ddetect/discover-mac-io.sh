f325d48841556c831a214330d3a6ea6ad2e2372c
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-09-19 14:34:02 +0000

    Detect mace Ethernet controller. Detect mac53c94 SCSI controller. Detect therm_adt746x and therm_windtunnel fan controllers, although only post-reboot for now since those modules aren't in linux-kernel-di-powerpc-*.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@21860 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/ddetect/discover-mac-io.sh
+++ packages/ddetect/discover-mac-io.sh
@@ -18,12 +18,38 @@ for dir in $(find "$macio" -type d); do
 	device_type="$(cat "$dir/device_type" 2>/dev/null || true)"
 	compatible="$(cat "$dir/compatible" 2>/dev/null || true)"
 
-	if [ "$name" = radio ]; then
+	# drivers/macintosh
+	if [ "$name" = fan ]; then
+		case $compatible in
+			adt7460|adt7467)
+				# not in d-i yet
+				# echo "therm_adt746x:iBook/AlBook G4 ADT746x thermostat"
+				register-module therm_adt746x
+				;;
+			adm1030)
+				# not in d-i yet
+				# echo "therm_windtunnel:G4 Windtunnel thermostat"
+				register-module therm_windtunnel
+				;;
+		esac
+	# drivers/net
+	elif [ "$name" = radio ]; then
 		echo "airport:Airport wireless"
 		register-module airport
 	elif [ "$name" = bmac ] || ([ "$device_type" = network ] && [ "$compatible" = bmac+ ]); then
 		echo "bmac:PowerMac BMAC Ethernet"
 		register-module bmac
+	elif [ "$name" = mace ]; then
+		echo "mace:PowerMac MACE Ethernet"
+		register-module mace
+	# drivers/scsi
+	elif [ "$name" = 53c94 ]; then
+		echo "mac53c94:PowerMac 53c94 SCSI bus adaptor"
+		register-module mac53c94
+	elif [ "$name" = mesh ] || ([ "$device_type" = scsi ] && [ "$compatible" = chrp,mesh0 ]); then
+		echo "mesh:Macintosh Enhanced SCSI Hardware"
+		register-module mesh
+	# sound/ppc, sound/oss/dmasound
 	elif [ "$name" = awacs ]; then
 		case "$(uname -r)" in
 		2.4*)
@@ -51,8 +77,5 @@ for dir in $(find "$macio" -type d); do
 				fi
 			fi
 		done
-	elif [ "$name" = mesh ] || ([ "$device_type" = scsi ] && [ "$compatible" = chrp,mesh0 ]); then
-		echo "mesh:Macintosh Enhanced SCSI Hardware"
-		register-module mesh
 	fi
 done
