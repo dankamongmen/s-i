165614b5fea2f5ee8f8fbb11db82e82db3b3172a
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2004-09-30 16:27:19 +0000

    Look through all of /proc/device-tree/, not just the mac-io bus. Slower but apparently necessary for at least the therm_* fan controllers, which are on uni-n here.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@22353 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/ddetect/discover-mac-io.sh
+++ packages/ddetect/discover-mac-io.sh
@@ -1,19 +1,14 @@
 #! /bin/sh
 set -e
 
-# Detect hardware on the mac-io bus. This is a grab-bag of stuff at the
-# moment; it should really move into discover or be hotpluggable.
+# Detect hardware from Open Firmware's exported device tree, including
+# things on the mac-io bus. This is a grab-bag of stuff at the moment; it
+# should really move into discover or be hotpluggable.
 #
 # If the hardware is of use within d-i, then echo it and register-module it;
 # otherwise, just use register-module.
 
-if [ -f /proc/device-tree/aliases/mac-io ]; then
-	macio="/proc/device-tree$(cat /proc/device-tree/aliases/mac-io)"
-else
-	exit 0
-fi
-
-for dir in $(find "$macio/" -type d); do
+for dir in $(find "/proc/device-tree/" -type d); do
 	name="$(cat "$dir/name" 2>/dev/null || true)"
 	device_type="$(cat "$dir/device_type" 2>/dev/null || true)"
 	compatible="$(cat "$dir/compatible" 2>/dev/null || true)"
