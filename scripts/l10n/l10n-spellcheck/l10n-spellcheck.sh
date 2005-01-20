d9621b71adc72f31e573aab67a7143742309e42f
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-01-20 23:17:44 +0000

    Removed various "cd" in order to make the script more secure.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@25055 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/l10n-spellcheck.sh
+++ scripts/l10n/l10n-spellcheck/l10n-spellcheck.sh
@@ -11,37 +11,31 @@ PATH=~/aspell:~/gnuplot/bin:$PATH
 export PATH
 
 LOCAL_REPOSITORY=~/debian-installer
-NEW="check_$(date '+%Y%m%d')"
 OUT_DIR=~/public_html/spellcheck/
+NEW="check_$(date '+%Y%m%d')"
 
 # update local copy of svn repository
-cd $LOCAL_REPOSITORY
-svn up
+svn up $LOCAL_REPOSITORY
 
 # spellcheck and move data to public_html
-cd ~/l10n-spellcheck
 sh check_all.sh $LOCAL_REPOSITORY
 mv $NEW $OUT_DIR
 
-cd  $OUT_DIR
-
 # save symlink destinations
-PREVIOUS=`ls -l previous | sed "s:.*-> ::"`
-LATEST=`ls -l latest | sed "s:.*-> ::"`
+PREVIOUS=`ls -l $OUT_DIR/previous | sed "s:.*-> ::"`
+LATEST=`ls -l $OUT_DIR/latest | sed "s:.*-> ::"`
 SAVED_STATS=`echo ${NEW}.txt | sed "s:check_:stats_:"`
 
-rm previous
-rm -r $PREVIOUS
-rm latest
+rm $OUT_DIR/previous
+rm -r $OUT_DIR/$PREVIOUS
 
-ln -s $NEW latest
-ln -s $LATEST previous
+rm $OUT_DIR/latest
 
-cp $NEW/stats.txt history/$SAVED_STATS
+ln -s $OUT_DIR/$NEW    $OUT_DIR/latest
+ln -s $OUT_DIR/$LATEST $OUT_DIR/previous
 
-mv $NEW/index.html .
-
-cd ~/l10n-spellcheck
+cp $OUT_DIR/$NEW/stats.txt $OUT_DIR/history/$SAVED_STATS
+mv $OUT_DIR/$NEW/index.html $OUT_DIR
 
 echo ""
 echo "***  $SAVED_STATS  ***"
