f4bae53c7393a3ba8e653b2f7a4a94e28a3dde1d
bubulle <bubulle@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-01-30 21:59:13 +0000

    New version of Denis Barbier script. Allows skipping the report of swapped entries.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@25328 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/check_var.pl
+++ scripts/l10n/check_var.pl
@@ -1,13 +1,22 @@
-#! /usr/bin/perl
+#! /usr/bin/perl -w
+
+use strict;
+use Getopt::Long qw(GetOptions);
+
+my $sort = 0;
+GetOptions(
+        'sort|s' => \$sort
+);
 
 sub getVars
 {
         my $text = shift;
-        my $var = '';
+        my @vars = ();
         while ($text =~ m/\G.*?(\$\{[^{}]+\})/g) {
-                $var .= $1;
+                push @vars, $1;
         }
-        return $var;
+        @vars = sort (@vars) if $sort;
+        return join ('', @vars);
 }
 
 $/ = "\n\n";
@@ -15,9 +24,11 @@ open (PO, "< $ARGV[0]") or die "Unable to open $ARGV[0]: $!\n";
 while (<PO>)
 {
         s/"\n"//g;
+        next if m/^#, .*fuzzy/m;
+        next unless m/^msgid/m;
         (my $msgid) = m/^msgid "(.*)"$/m;
         (my $msgstr) = m/^msgstr "(.*)"$/m;
-        next if $msgstr eq '' || m/^#, .*fuzzy/m;
+        next if $msgstr eq '';
         my $var1 = getVars($msgid);
         my $var2 = getVars($msgstr);
         print if $var1 ne $var2;
