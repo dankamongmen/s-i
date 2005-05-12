7bcf17eeca93ea55259e3fa2388ed0db101adda0
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-05-12 22:54:29 +0000

    General improvement; it's almost possible to run two instances at the same time.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@27528 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/check_all.sh
+++ scripts/l10n/l10n-spellcheck/check_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# usage: check_all <D-I repository path>
+# usage: check_all <D-I repository path> <output dir>
 #
 # check_all - runs check_dit.sh on all the languages listed in lang2dict.txt
 #
@@ -11,20 +11,19 @@
 
 usage() {
     echo  "Usage:"
-    echo  "$0 <D-I repository path>"
+    echo  "$0 <D-I repository path> <output dir>"
 }
 
 initialise(){
-    DEST_DIR="check_$(date '+%Y%m%d')"
-    STATS=$DEST_DIR/stats
-    GNUPLOT_SCRIPT=$DEST_DIR/graph.gp
-    GNUPLOT_DATA=$DEST_DIR/data.txt
+    STATS=${DEST_DIR}/stats
+    GNUPLOT_SCRIPT=${DEST_DIR}/graph.gp
+    GNUPLOT_DATA=${DEST_DIR}/data.txt
 }
 
 checks()
 {
-    if [ ! -d $DI_COPY ] ; then
-	echo $DI_COPY does not exist
+    if [ ! -d ${DI_COPY} ] ; then
+	echo ${DI_COPY} does not exist
 	exit 1
     fi
 }
@@ -36,6 +35,7 @@ if [ -z "$1" ]
 fi
 
 DI_COPY=$1
+DEST_DIR=$2
 
 initialise	# initalise some variables
 checks          # do an environment check
@@ -58,55 +58,56 @@ if [ ! -d ${DEST_DIR} ] ; then
 fi
 
 # build "index.html" with the new results
-sh build_index.sh $STATS.txt $HTML_PAGE $DEST_DIR/index.html
+sh build_index.sh ${STATS}.txt ${HTML_PAGE} ${DEST_DIR}/index.html
 
 # Compute some statistics
 i=0
 TOTAL=0
 AVERAGE=0
-for VAL in `cat $STATS.txt | sort -n | awk '{print $1}'`; do
-    TOTAL=`expr $TOTAL + $VAL`
+for VAL in `cat ${STATS}.txt | sort -n | awk '{print $1}'`; do
+    TOTAL=`expr ${TOTAL} + ${VAL}`
     i=`expr $i + 1`
 done
 
 # avoid division by 0
 if [ $i -ne 0 ] ; then
-    AVERAGE=`expr $TOTAL / $i`
+    AVERAGE=`expr ${TOTAL} / $i`
 fi
 
 # create plot using gnuplot
 i=0
-for ROW in `cat $STATS.txt | sort -n | awk '{print $2}'`; do
-    XTICS=$(echo "$XTICS \"$ROW\" $i,")
+for ROW in `cat ${STATS}.txt | sort -n | awk '{print $2}'`; do
+    XTICS=$(echo "${XTICS} \"${ROW}\" $i,")
     i=`expr $i + 1`
 done
 
-XTICS=`echo $XTICS | sed "s:^":\(":" | sed "s:,$:):"`
+XTICS=`echo ${XTICS} | sed "s:^":\(":" | sed "s:,$:):"`
 
-cat $STATS.txt | sort -n | awk '{print $1}' > $GNUPLOT_DATA
+cat ${STATS}.txt | sort -n | awk '{print $1}' > ${GNUPLOT_DATA}
 
-rm -f $GNUPLOT_SCRIPT
-LOGFILE=$GNUPLOT_SCRIPT
+rm -f ${GNUPLOT_SCRIPT}
+LOGFILE=${GNUPLOT_SCRIPT}
 exec 6>&1           # Link file descriptor #6 with stdout.
                     # Saves stdout.
 
-exec > $LOGFILE     # stdout replaced with file "logfile.txt".
+exec > ${LOGFILE}     # stdout replaced with file "logfile.txt".
 
 #echo "set terminal png size 640,480"
 echo "set terminal png"
-echo "set output \"$DEST_DIR/graph.png\""
-echo "set title \"$PLOT_TITLE ($i languages)\""
+echo "set output \"${DEST_DIR}/graph.png\""
+echo "set title \"${PLOT_TITLE} ($i languages)\""
 echo "set key left"
 echo "set xlabel \"Languages\" 0,-1"
 echo "set ylabel \"Unknown words\""
 echo "set origin 0,0.01"
-echo "set xtics rotate $XTICS"
+echo "set xtics rotate ${XTICS}"
 echo "set xrange [-1:$i]"
-echo "plot \"$GNUPLOT_DATA\" with impulses,$AVERAGE t \"Average: $AVERAGE words\""
+echo "plot \"${GNUPLOT_DATA}\" with impulses,${AVERAGE} t \"Average: ${AVERAGE} words\""
 
 exec 1>&6 6>&-      # Restore stdout and close file descriptor #6.
 
-gnuplot $GNUPLOT_SCRIPT
+gnuplot ${GNUPLOT_SCRIPT}
 
-rm -f $GNUPLOT_DATA
-rm -f $GNUPLOT_SCRIPT
+rm -f ${GNUPLOT_DATA}
+rm -f ${GNUPLOT_SCRIPT}
+
--- scripts/l10n/l10n-spellcheck/l10n-spellcheck.sh
+++ scripts/l10n/l10n-spellcheck/l10n-spellcheck.sh
@@ -7,11 +7,11 @@
 # Author: Davide Viti <zinosat@tiscali.it> 2005, for the Debian Project
 #
 
-export PATH=${HOME}/aspell:${HOME}/gnuplot/bin:$PATH
+export PATH=${HOME}/aspell:${HOME}/gnuplot/bin:${PATH}
 SCRIPT_WITHOUT_PATH=`basename $0`
 SCRIPTS_PATH=`echo $0 | sed "s:\(.*\)${SCRIPT_WITHOUT_PATH}\(.*\):\1\2:"`
 
-. ${HOME}/$SCRIPTS_PATH/cfg/$1/setup.sh
+. ${HOME}/${SCRIPTS_PATH}/cfg/$1/setup.sh
 export LANGUAGE_LIST="cfg/$1/lang2dict.txt"
 export PO_FINDER="cfg/$1/po_finder.sh"
 export HTML_PAGE="cfg/$1/report_page.html"
@@ -27,28 +27,28 @@ NEW="check_$(date '+%Y%m%d')"
 svn up ${LOCAL_REPOSITORY}
 
 # spellcheck and move data to public_html
-cd  ${HOME}/$SCRIPTS_PATH
-sh check_all.sh ${LOCAL_REPOSITORY}
-mv $NEW $OUT_DIR
+cd  ${HOME}/${SCRIPTS_PATH}
+sh check_all.sh ${LOCAL_REPOSITORY} ${NEW}_$1
+mv ${NEW}_$1 ${OUT_DIR}/${NEW}
 
 # save symlink destinations
-PREVIOUS=`ls -l $OUT_DIR/previous | sed "s:.*-> ::"`
-LATEST=`ls -l $OUT_DIR/latest | sed "s:.*-> ::"`
+PREVIOUS=`ls -l ${OUT_DIR}/previous | sed "s:.*-> ::"`
+LATEST=`ls -l ${OUT_DIR}/latest | sed "s:.*-> ::"`
 SAVED_STATS=`echo ${NEW}.txt | sed "s:check_:stats_:"`
 
-rm $OUT_DIR/previous
-rm $OUT_DIR/latest
+rm ${OUT_DIR}/previous
+rm ${OUT_DIR}/latest
 
-ln -s $OUT_DIR/$NEW    $OUT_DIR/latest
-ln -s $LATEST          $OUT_DIR/previous
+ln -s ${OUT_DIR}/${NEW}    ${OUT_DIR}/latest
+ln -s ${LATEST}          ${OUT_DIR}/previous
 
 # purge old entry
-rm -fr $PREVIOUS
+rm -fr ${PREVIOUS}
 
-cp $OUT_DIR/latest/stats.txt $OUT_DIR/history/$SAVED_STATS
-mv $OUT_DIR/latest/index.html $OUT_DIR
+cp ${OUT_DIR}/latest/stats.txt ${OUT_DIR}/history/${SAVED_STATS}
+mv ${OUT_DIR}/latest/index.html ${OUT_DIR}
 
 echo ""
 echo "***  $SAVED_STATS  ***"
 
-sh diff_stats.sh $OUT_DIR/previous/stats.txt $OUT_DIR/latest/stats.txt
+sh diff_stats.sh ${OUT_DIR}/previous/stats.txt ${OUT_DIR}/latest/stats.txt
