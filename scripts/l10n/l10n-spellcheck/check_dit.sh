79041f5bf7dd25f66e4dddb75d1ca814ba012f22
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-05-14 09:37:17 +0000

    It's now possible to run more instances of the spellchecker at the same time. Code cleanup.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@27557 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/check_dit.sh
+++ scripts/l10n/l10n-spellcheck/check_dit.sh
@@ -19,44 +19,44 @@ GATHER_MSGSTR_SCRIPT=./msgstr_extract.awk
 GATHER_MSGID_SCRIPT=./msgid_extract.awk
 CHECK_VAR=./check_var.pl
 
-ALL_STRINGS=$DEST_DIR/${LANG}_all.txt
-FILES_TO_KEEP="$ALL_STRINGS $FILES_TO_KEEP"
+ALL_STRINGS=${DEST_DIR}/${LANG}_all.txt
+FILES_TO_KEEP="${ALL_STRINGS} ${FILES_TO_KEEP}"
 
-NO_VARS=$DEST_DIR/1_no_vars_${LANG}
+NO_VARS=${DEST_DIR}/1_no_vars_${LANG}
 
-ALL_UNKNOWN=$DEST_DIR/2_all_unkn_${LANG}
-NEEDS_RM="$ALL_UNKNOWN $NEEDS_RM"
+ALL_UNKNOWN=${DEST_DIR}/2_all_unkn_${LANG}
+NEEDS_RM="${ALL_UNKNOWN} ${NEEDS_RM}"
 
-UNKN=$DEST_DIR/${LANG}_unkn_wl.txt
-FILES_TO_KEEP="$UNKN $FILES_TO_KEEP"
+UNKN=${DEST_DIR}/${LANG}_unkn_wl.txt
+FILES_TO_KEEP="${UNKN} ${FILES_TO_KEEP}"
 
-SUSPECT_VARS=$DEST_DIR/${LANG}_var.txt
+SUSPECT_VARS=${DEST_DIR}/${LANG}_var.txt
 }
 
 checks(){
 
-if [ ! -d $BASE_SEARCH_DIR ] ; then
-    echo $BASE_SEARCH_DIR does not exist
+if [ ! -d ${BASE_SEARCH_DIR} ] ; then
+    echo ${BASE_SEARCH_DIR} does not exist
     exit 1
 fi
 
-if [ ! -f $GATHER_MSGSTR_SCRIPT ] ; then
-    echo "$GATHER_MSGSTR_SCRIPT does not exist. You need it!"
+if [ ! -f ${GATHER_MSGSTR_SCRIPT} ] ; then
+    echo "${GATHER_MSGSTR_SCRIPT} does not exist. You need it!"
     exit 1
 fi
 
-if [ ! -f $GATHER_MSGID_SCRIPT ] ; then
-    echo "$GATHER_MSGID_SCRIPT does not exist. You need it!"
+if [ ! -f ${GATHER_MSGID_SCRIPT} ] ; then
+    echo "${GATHER_MSGID_SCRIPT} does not exist. You need it!"
     exit 1
 fi
 
-if [ ! -f $CHECK_VAR ] ; then
-    echo "$CHECK_VAR does not exist. You need it!"
+if [ ! -f ${CHECK_VAR} ] ; then
+    echo "${CHECK_VAR} does not exist. You need it!"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ] ; then
-    mkdir $DEST_DIR
+if [ ! -d ${DEST_DIR} ] ; then
+    mkdir ${DEST_DIR}
 fi
 
 }
@@ -75,56 +75,56 @@ DEST_DIR=$4
 initialise	# initalise some variables
 checks		# do an environment check
 
-PO_FILE_LIST="${LANG}_file_list.txt"
-NEEDS_RM="$PO_FILE_LIST $NEEDS_RM"
+PO_FILE_LIST="${DEST_DIR}/${LANG}_file_list.txt"
+NEEDS_RM="${PO_FILE_LIST} ${NEEDS_RM}"
 
-sh $PO_FINDER $BASE_SEARCH_DIR $LANG > $PO_FILE_LIST
+sh ${PO_FINDER} ${BASE_SEARCH_DIR} ${LANG} > ${PO_FILE_LIST}
 
-rm -f $ALL_STRINGS
-for LANG_FILE in `cat $PO_FILE_LIST`; do
-    ENC=`cat $LANG_FILE | grep -e "^\"Content-Type:" | sed 's:^.*charset=::' | sed 's:\\\n\"::'`
+rm -f ${ALL_STRINGS}
+for LANG_FILE in `cat ${PO_FILE_LIST}`; do
+    ENC=`cat ${LANG_FILE} | grep -e "^\"Content-Type:" | sed 's:^.*charset=::' | sed 's:\\\n\"::'`
 
-    echo "$LANG_FILE" | grep -e ".po$" > /dev/null
+    echo "${LANG_FILE}" | grep -e ".po$" > /dev/null
     if  [ $? = 0 ] ; then
 
-	echo $ENC | grep -iw "utf-8" > /dev/null
+	echo ${ENC} | grep -iw "utf-8" > /dev/null
 	if [ $? = 0 ]  ; then
-	    if [ $HANDLE_SUSPECT_VARS = "yes" ] ; then
-		$CHECK_VAR -s $LANG_FILE >> $SUSPECT_VARS
+	    if [ ${HANDLE_SUSPECT_VARS} = "yes" ] ; then
+		${CHECK_VAR} -s ${LANG_FILE} >> ${SUSPECT_VARS}
 	    fi
-	    awk -f $GATHER_MSGSTR_SCRIPT $LANG_FILE >> $ALL_STRINGS
+	    awk -f ${GATHER_MSGSTR_SCRIPT} ${LANG_FILE} >> ${ALL_STRINGS}
 	else
-	    if [ $HANDLE_SUSPECT_VARS = "yes" ] ; then
-		$CHECK_VAR -s $LANG_FILE | iconv --from $ENC --to utf-8 >> $SUSPECT_VARS
+	    if [ ${HANDLE_SUSPECT_VARS} = "yes" ] ; then
+		${CHECK_VAR} -s ${LANG_FILE} | iconv --from ${ENC} --to utf-8 >> ${SUSPECT_VARS}
 	    fi
-	    awk -f $GATHER_MSGSTR_SCRIPT $LANG_FILE | iconv --from $ENC --to utf-8 >> $ALL_STRINGS
+	    awk -f ${GATHER_MSGSTR_SCRIPT} ${LANG_FILE} | iconv --from ${ENC} --to utf-8 >> ${ALL_STRINGS}
 	fi
 
     else			# now deal with ".pot" files
-	if [ $HANDLE_SUSPECT_VARS = "yes" ] ; then
-	    $CHECK_VAR -s $LANG_FILE >> $SUSPECT_VARS
+	if [ ${HANDLE_SUSPECT_VARS} = "yes" ] ; then
+	    ${CHECK_VAR} -s ${LANG_FILE} >> ${SUSPECT_VARS}
 	fi
-	awk -f $GATHER_MSGID_SCRIPT $LANG_FILE >> $ALL_STRINGS
+	awk -f ${GATHER_MSGID_SCRIPT} ${LANG_FILE} >> ${ALL_STRINGS}
     fi
 done
 
-if [ $HANDLE_SUSPECT_VARS = "yes" ] ; then
-    if [ `ls -l $SUSPECT_VARS | awk '{print $5}'` -gt 0 ]; then
-	FILES_TO_KEEP="$SUSPECT_VARS $FILES_TO_KEEP" 
+if [ ${HANDLE_SUSPECT_VARS} = "yes" ] ; then
+    if [ `ls -l ${SUSPECT_VARS} | awk '{print $5}'` -gt 0 ]; then
+	FILES_TO_KEEP="${SUSPECT_VARS} ${FILES_TO_KEEP}" 
 	SUSPECT_EXIST=1
     else
-	rm $SUSPECT_VARS
+	rm ${SUSPECT_VARS}
 	SUSPECT_EXIST=0
     fi
 fi
 
 # remove ${ALL_THESE_VARIABLES} if they do not need to be spell checked
-if [ $REMOVE_VARS = "yes" ] ; then
-    NEEDS_RM="$NO_VARS $NEEDS_RM"
-    grep -e "^-" $ALL_STRINGS | sed s/\$\{[a-zA-Z0-9_]*\}//g > $NO_VARS
-    FILE_TO_CHECK=$NO_VARS
+if [ ${REMOVE_VARS} = "yes" ] ; then
+    NEEDS_RM="${NO_VARS} ${NEEDS_RM}"
+    grep -e "^-" ${ALL_STRINGS} | sed s/\$\{[a-zA-Z0-9_]*\}//g > ${NO_VARS}
+    FILE_TO_CHECK=${NO_VARS}
 else
-    FILE_TO_CHECK=$ALL_STRINGS
+    FILE_TO_CHECK=${ALL_STRINGS}
 fi
 
 # if a binary wl exists, use it
@@ -133,36 +133,36 @@ if [ -f ./wls/${LANG}_di_wl ] ; then
 fi
 
 # spell check the selected strings eventually using a custom wl 
-cat $FILE_TO_CHECK | aspell list --lang=$LANG --encoding=utf-8 $WL_PARAM $ASPELL_EXTRA_PARAM > $ALL_UNKNOWN
+cat ${FILE_TO_CHECK} | aspell list --lang=${LANG} --encoding=utf-8 ${WL_PARAM} ${ASPELL_EXTRA_PARAM} > ${ALL_UNKNOWN}
 
 # sort all the unrecognized words (don't care about upper/lower case)
 # count duplicates
 # take note of unknown words
-cat $ALL_UNKNOWN | sort -f | uniq -c > $UNKN
+cat ${ALL_UNKNOWN} | sort -f | uniq -c > ${UNKN}
 
 # if we're *not* handling suspecet vars (i.e. d-i manual), make this an empty string
-if [ $HANDLE_SUSPECT_VARS = "no" ] ; then
+if [ ${HANDLE_SUSPECT_VARS} = "no" ] ; then
     SUSPECT_EXIST=
 fi
 
 # build the entry of stats.txt for the current language (i.e "395 it 1")
-echo `wc -l $UNKN | awk '{print $1}'` $LANG $SUSPECT_EXIST >> ${DEST_DIR}/stats.txt
+echo `wc -l ${UNKN} | awk '{print $1}'` ${LANG} ${SUSPECT_EXIST} >> ${DEST_DIR}/stats.txt
 
-rm $NEEDS_RM
+rm ${NEEDS_RM}
 
-if [ ! -d  $DEST_DIR/zip ] ; then
-    mkdir $DEST_DIR/zip
+if [ ! -d  ${DEST_DIR}/zip ] ; then
+    mkdir ${DEST_DIR}/zip
 fi
 
-if [ ! -d  $DEST_DIR/nozip ] ; then
-    mkdir $DEST_DIR/nozip
+if [ ! -d  ${DEST_DIR}/nozip ] ; then
+    mkdir ${DEST_DIR}/nozip
 fi
 
 # in the tgz file WL is iso-8859-1 (the way it has to be)
 # in "nozip" it's utf-8 like the other files 
 if [ -f ./wls/${LANG}_wl.txt ] ; then
     WORDLIST=./wls/${LANG}_wl.txt
-    cat ${WORDLIST} | iconv --from iso-8859-1 --to utf-8 > $DEST_DIR/nozip/${LANG}_wl.txt
+    cat ${WORDLIST} | iconv --from iso-8859-1 --to utf-8 > ${DEST_DIR}/nozip/${LANG}_wl.txt
     cp ${WORDLIST} ${DEST_DIR}
     WL_ISO8859=${DEST_DIR}/${LANG}_wl.txt
 fi
@@ -172,11 +172,9 @@ if [ -f ./wls/di_common_wl.txt ] ; then
     cp ./wls/di_common_wl.txt ${DEST_DIR}/nozip
 fi
 
-tar czf ${DEST_DIR}/${LANG}.tar.gz $FILES_TO_KEEP ${WL_ISO8859}
+tar czf ${DEST_DIR}/zip/${LANG}.tar.gz ${FILES_TO_KEEP} ${WL_ISO8859}
 
-mv ${DEST_DIR}/${LANG}.tar.gz ${DEST_DIR}/zip
-
-mv $FILES_TO_KEEP ${DEST_DIR}/nozip
+mv ${FILES_TO_KEEP} ${DEST_DIR}/nozip
 rm -f ${WL_ISO8859}
 
 echo "AddCharset UTF-8 .txt" > ${DEST_DIR}/nozip/.htaccess
