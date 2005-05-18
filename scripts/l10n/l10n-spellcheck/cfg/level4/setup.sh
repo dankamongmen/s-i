b39e65094635a51a5d8f30a6f7b9a6cf62400df3
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-05-18 15:58:54 +0000

    Fixed bug in setup file for level4 spellcheck
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@27688 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/cfg/level4/setup.sh
+++ scripts/l10n/l10n-spellcheck/cfg/level4/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # *** l10n-spellcheck.sh ***
-export LOCAL_REPOSITORY="${HOME}/d-i/levels&level4"
+export LOCAL_REPOSITORY="${HOME}/d-i/levels/level4"
 export OUT_DIR="${HOME}/public_html/spellcheck/level4/"
 
 # remove ${ALL_THESE_VARIABLES} which do not need to be spell checked
