c5506b226aef15a29fb7884b21903de491f8d86b
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-05-19 21:48:16 +0000

    Instead of keeping results for old and new check, only new files are kept along with diff files (not available on the page yet). This will save many MBs on the server and simplify things.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@27732 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/l10n-spellcheck.sh
+++ scripts/l10n/l10n-spellcheck/l10n-spellcheck.sh
@@ -27,30 +27,48 @@ NEW="check_$(date '+%Y%m%d')"
 # update local copy of the repository (if necessary)
 ${REFRESH_CMD}
 
-
 # spellcheck and move data to public_html
 cd  ${HOME}/${SCRIPTS_PATH}
 sh check_all.sh ${LOCAL_REPOSITORY} ${NEW}_$1
-mv ${NEW}_$1 ${OUT_DIR}/${NEW}
 
-# save symlink destinations
-PREVIOUS=`ls -l ${OUT_DIR}/previous | sed "s:.*-> ::"`
-LATEST=`ls -l ${OUT_DIR}/latest | sed "s:.*-> ::"`
 SAVED_STATS=`echo ${NEW}.txt | sed "s:check_:stats_:"`
+cp ${NEW}_$1/stats.txt ${OUT_DIR}/history/${SAVED_STATS}
+mv ${NEW}_$1/index.html ${OUT_DIR}
 
-rm ${OUT_DIR}/previous
-rm ${OUT_DIR}/latest
+OLD_PATH=${OUT_DIR}/latest/nozip
+NEW_PATH=${NEW}_$1/nozip
 
-ln -s ${OUT_DIR}/${NEW}    ${OUT_DIR}/latest
-ln -s ${LATEST}          ${OUT_DIR}/previous
+for ROW in `cat ${NEW}_$1/stats.txt | sed 's: :,:g'`; do
+    VAL=`echo ${ROW}| awk -F, '{print $1}'`
+    LANG=`echo ${ROW}| awk -F, '{print $2}'`
 
-# purge old entry
-rm -fr ${PREVIOUS}
+    LALL=${LANG}_all.txt
+    LUWL=${LANG}_unkn_wl.txt
+    LVAR=${LANG}_var.txt
+  
+    for TO_DIFF in ${LALL} ${LUWL} ${LVAR} ; do
+	if [ -f ${OLD_PATH}/${TO_DIFF} -o -f ${NEW_PATH}/${TO_DIFF} ] ; then
 
-cp ${OUT_DIR}/latest/stats.txt ${OUT_DIR}/history/${SAVED_STATS}
-mv ${OUT_DIR}/latest/index.html ${OUT_DIR}
+	    diff -u0 -N \
+		--label old ${OLD_PATH}/${TO_DIFF} \
+		--label new ${NEW_PATH}/${TO_DIFF} > \
+		${NEW_PATH}/${TO_DIFF/.txt/.diff}
+	fi
+    done
+
+done
+
+# remove empty files
+find ${NEW_PATH} -empty -exec rm '{}' ';'
 
 echo ""
 echo "***  $SAVED_STATS  ***"
 
-sh diff_stats.sh ${OUT_DIR}/previous/stats.txt ${OUT_DIR}/latest/stats.txt
+sh diff_stats.sh ${OUT_DIR}/latest/stats.txt ${NEW}_$1/stats.txt
+
+LATEST=`ls -l ${OUT_DIR}/latest | sed "s:.*-> ::"`
+rm ${OUT_DIR}/latest 
+rm -fr $LATEST
+
+mv ${NEW}_$1 ${OUT_DIR}/${NEW}
+ln -s ${OUT_DIR}/${NEW}  ${OUT_DIR}/latest
