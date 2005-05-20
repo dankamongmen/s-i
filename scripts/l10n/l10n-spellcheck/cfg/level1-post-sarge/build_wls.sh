aacfc83cc8b1bbe154c449c7670ee33f181103f2
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-05-20 17:39:42 +0000

    "sl" added back to level4. Improved method of rebuilding wls.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@27766 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/cfg/level1-post-sarge/build_wls.sh
+++ scripts/l10n/l10n-spellcheck/cfg/level1-post-sarge/build_wls.sh
@@ -13,32 +13,32 @@ COMMON_WL=./wls/di_common_wl.txt
 for LANGUAGE in `cat ${LANGUAGE_LIST} | sed "s:\(^#.*\)::"`; do
     LANG=`echo  ${LANGUAGE} | awk -F, '{print $1}'`
     DICT=`echo  ${LANGUAGE} | awk -F, '{print $2}'`
-
-    SPECIFIC_WL=./wls/${LANG}_wl.txt
-    WLIST=./wls/${LANG}_di_wl
-
-    WL_WARN=0
-    if [ ! -f ${COMMON_WL} ] ; then
-	echo "can't find di_common_wl.txt. You'll get a lot of unknown technical words!"
-	COMMON_WL=
-	WL_WARN=`expr ${WL_WARN} + 1`
-    fi
-
-    if [ ! -f ${SPECIFIC_WL} ] ; then
-	echo "can't find ${LANG}_wl.txt. You'll get a lot of unknown words in \"${LANG}\" language!"
-	SPECIFIC_WL=
-	WL_WARN=`expr ${WL_WARN} + 1`    
-    fi
-
+    
+    if [ ${#DICT} -gt 0 ] ; then    
+	SPECIFIC_WL=./wls/${LANG}_wl.txt
+	WLIST=./wls/${LANG}_di_wl
+    
+	WL_WARN=0
+	if [ ! -f ${COMMON_WL} ] ; then
+	    echo "can't find di_common_wl.txt. You'll get a lot of unknown technical words!"
+	    COMMON_WL=
+	    WL_WARN=`expr ${WL_WARN} + 1`
+	fi
+	
+	if [ ! -f ${SPECIFIC_WL} ] ; then
+	    echo "can't find ${LANG}_wl.txt. You'll get a lot of unknown words in \"${LANG}\" language!"
+	    SPECIFIC_WL=
+	    WL_WARN=`expr ${WL_WARN} + 1`    
+	fi
+	
 # build wl only if there's at least one of the "private" wls
 # "| perl -00lne'/\n/&&print'" would remove blank lines from wl: is it needed?
-    if [ ${WL_WARN} -ne 2 ] ; then
-	cat ${COMMON_WL} ${SPECIFIC_WL} | sort -f | sed "s:\(^#.*\)::" > ${WLIST}.txt
-
+	if [ ${WL_WARN} -ne 2 ] ; then
+	    cat ${COMMON_WL} ${SPECIFIC_WL} | sort -f | sed "s:\(^#.*\)::" > ${WLIST}.txt
+	    
 # NB: --lang uses $LANG and not $DICT
-	aspell --lang=${LANG} create master ${WLIST} < ${WLIST}.txt
-	rm ${WLIST}.txt
+	    aspell --lang=${LANG} create master ${WLIST} < ${WLIST}.txt
+	    rm ${WLIST}.txt
+	fi	
     fi
-
-
 done
