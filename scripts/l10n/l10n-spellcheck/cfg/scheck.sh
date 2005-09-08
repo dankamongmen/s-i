93073dcb6c2336ea8d9becfd1d22ad2c58a8e0b5
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-09-08 22:55:34 +0000

    This script is used on Alioth to trigger the check of a particular level
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@30437 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ scripts/l10n/l10n-spellcheck/cfg/scheck.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+export PATH=${HOME}/aspell:${HOME}/gnuplot/bin:${HOME}/l10n-spellcheck:$PATH
+
+case "$1" in
+man)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/manual_d-i
+	;;
+l1-ps)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/level1-post-sarge
+	;;
+l1)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/level1
+	;;
+l2)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/level2
+	;;
+l3)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/level3
+	;;
+l4)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/level4
+	;;
+l5)
+	l10n-spellcheck.sh ~/l10n-spellcheck/cfg/level5
+	;;
+esac
