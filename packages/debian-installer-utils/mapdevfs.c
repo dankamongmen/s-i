27baeb2df1d1e1bacb56131bcab7554a771ecd23
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-10-07 03:35:52 +0000

    * Indentation consistency.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@31215 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/debian-installer-utils/mapdevfs.c
+++ packages/debian-installer-utils/mapdevfs.c
@@ -6,18 +6,18 @@
 #include <debian-installer.h>
 
 int main (int argc, char **argv) {
-    static char buf[256];
-    size_t len = sizeof(buf);
+	static char buf[256];
+	size_t len = sizeof(buf);
 
-    if (argc != 2) {
-        fprintf(stderr, "Wrong number of args: mapdevfs <path>\n");
-        return 1;
-    }
+	if (argc != 2) {
+		fprintf(stderr, "Wrong number of args: mapdevfs <path>\n");
+		return 1;
+	}
 
-    if ((di_system_devfs_map_from(argv[1], buf, len))) {
-        printf("%s\n", buf);
-        return 0;
-    }
+	if ((di_system_devfs_map_from(argv[1], buf, len))) {
+		printf("%s\n", buf);
+		return 0;
+	}
 
-    return 1;
+	return 1;
 }
