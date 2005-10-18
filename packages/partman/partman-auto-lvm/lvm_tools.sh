e7f15a7edf35fe2dd9bfbcabb497762d181c5f1c
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-10-18 19:26:09 +0000

    merge ubuntu chnges from Fabio's ptch, with some manual merging needed
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@31454 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/partman/partman-auto-lvm/lvm_tools.sh
+++ packages/partman/partman-auto-lvm/lvm_tools.sh
@@ -1,16 +1,9 @@
 
 TAB='	'
 
-# Print a list of the names of all volume groups, one per line
-# VG name<TAB>size
+# Print a list of the names of all volume groups and their size in M (including decimals!)
 VG_list () {
-    local name size
-    vgdisplay | grep '^[ ]*VG Name' | sed -e 's/.*[[:space:]]\+\(.*\)$/\1/' | sort | 
-    while read name; do
-	# TODO: the size is in B, KB or MB?
-	size=$(vgdisplay "$name" 2>&1 | grep '^[ ]*VG Size' | sed -e 's/^[ ]*VG Size \+//')
-	echo "${name}${TAB}${size}"
-    done
+	vgs -o vg_name,vg_size --nosuffix --units M --noheadings --sort vg_name --separator "${TAB}"
 }
 
 # Create a new VG from a list of PV.  The PV will be pvcreate-d
@@ -25,27 +18,10 @@ VG_create () {
     return 0
 }
 
-# Print the name of the VG of a PV; exit 1 if this is not a phisical
-# volume or the information is not available
+# Print the name of the VG of which a PV belongs to.
 # $1 = the name of the PV
 VG_of_PV () {
-    local cmdout
-    cmdout=`pvdisplay "$1" 2>&1`
-    if echo "$cmdout" | grep -q 'is a new physical volume'; then
-	return 1
-    fi
-    
-    if echo "$cmdout" | grep -q '^pvdisplay'; then
-	return 1
-    fi
-    VG=`echo "$cmdout" | grep '^[ ]*VG Name' | \
-                sed -e 's/^[ ]*VG Name \+//'`
-    if [ "$VG" != "" ]; then
-	echo "$VG"
-	return 0
-    else
-	return 1
-    fi
+    pvs -o vg_name $1 --noheadings --separator "${TAB}" 2>/dev/null || return 1
 }
 
 # Make a PV not to be part of its VG
@@ -69,13 +45,16 @@ VG_extend () {
 # $2 = size in bytes
 # $3 = name of the LV to create
 LV_create () {
-#    if [ "$2" = full ]; then
-# Using full VG not implemented until we have a way to get the free size of
-# a VG
-#	log-output -t partman-auto-lvm lvcreate -l$() -n $3 $1
-#   else
-	log-output -t partman-auto-lvm lvcreate -L${2%???}k -n $3 $1
-#    fi
+    local size
+    if [ "$2" = full ]; then
+	size=$(vgs -o vg_free --noheadings --nosuffix --units k $1 | sed -e 's/\..*//g')
+	if [ "$size" -le "0" ] || [ -z "$size" ]; then
+	    return 1
+	fi
+    else
+	size=${2%???}
+    fi
+    log-output -t partman-auto-lvm lvcreate -L${size}k -n $3 $1
 }
 
 # Delete a LV
@@ -89,14 +68,5 @@ LV_remove () {
 # VG name<TAB>size
 # $1 = VG
 LV_list () {
-    local i cmdout size
-    for i in \
-        $(vgdisplay -v $1 | grep '^[ ]*LV Name' |
-	    sed -e 's,.*/\(.*\),\1,' | sort)
-    do
-	cmdout=`lvdisplay "$1" 2>&1`
-        size=`echo "$cmdout" | grep '^[ ]*LV Size' | \
-                sed -e 's/^[ ]*LV Size \+\(.*\)/\1/'`
-	echo "${i}${TAB}${size}"
-    done
+    lvs $1 -o lv_name,lv_size --nosuffix --units M --noheadings --sort lv_name --separator "${TAB}"
 }
