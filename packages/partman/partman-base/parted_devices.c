45a6113cc8bf4788cfce5007d8456079907390c6
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-11-17 20:05:31 +0000

    * Exclude CD/DVD drives from the output of parted_devices.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@32224 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/partman/partman-base/parted_devices.c
+++ packages/partman/partman-base/parted_devices.c
@@ -1,4 +1,35 @@
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+/* from <linux/cdrom.h> */
+#define CDROM_GET_CAPABILITY	0x5331	/* get capabilities */
+#endif /* __linux__ */
+
 #include <parted/parted.h>
+
+#ifdef __linux__
+int
+is_cdrom(const char *path)
+{
+	int fd;
+	int ret;
+
+	fd = open(path, O_RDONLY | O_NONBLOCK);
+	ret = ioctl(fd, CDROM_GET_CAPABILITY, NULL);
+	close(fd);
+
+	if (ret >= 0)
+		return 1;
+	else
+		return 0;
+}
+#else /* !__linux__ */
+#define is_cdrom(path) 0
+#endif /* __linux__ */
 
 int
 main(int argc, char *argv[])
@@ -8,6 +39,8 @@ main(int argc, char *argv[])
         ped_device_probe_all();
         for (dev = NULL; NULL != (dev = ped_device_get_next(dev));) {
 		if (dev->read_only)
+			continue;
+		if (is_cdrom(dev->path))
 			continue;
                 printf("%s\t%lli\t%s\n",
                        dev->path,
