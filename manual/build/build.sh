613a1721f03369f240aa1594359227d0612e14c3
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2005-11-25 18:49:08 +0000

    - Move documentation on preseeding to separate appendix for etch - Use new, more flexible perl script to extract example preseed file from new appendix
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@32424 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- manual/build/build.sh
+++ manual/build/build.sh
@@ -69,14 +69,15 @@ for lang in $languages; do
 done
 
 if [ "$manual_release" = "etch" ] ; then
-    PRESEED="../en/appendix/example-preseed-etch.xml"
-    LCKEEP="-v lckeep=1"
+    PRESEED="../en/appendix/preseed.xml"
+    if [ -f $PRESEED ] && [ -f preseed.pl ] ; then
+	./preseed.pl -r $manual_release $PRESEED >$destination/example-preseed.txt
+    fi
 else
     PRESEED="../en/appendix/example-preseed-sarge.xml"
-    LCKEEP=""
-fi
-if [ -f $PRESEED ] && [ -f preseed.awk ] ; then
-    gawk -f preseed.awk $LCKEEP $PRESEED >$destination/example-preseed.txt
+    if [ -f $PRESEED ] && [ -f preseed.awk ] ; then
+	gawk -f preseed.awk $LCKEEP $PRESEED >$destination/example-preseed.txt
+    fi
 fi
 
 clear_po
