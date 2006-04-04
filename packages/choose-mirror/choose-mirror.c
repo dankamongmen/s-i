2916a9dd88eacf93b1acf85fa666c817b437e42a
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2006-04-04 16:26:55 +0000

    Revert previous commit
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@36096 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/choose-mirror/choose-mirror.c
+++ packages/choose-mirror/choose-mirror.c
@@ -195,18 +195,6 @@ int find_suite (void) {
 	return ret;
 }
 
-static int base_on_cd = 0;
-static int check_base_on_cd(void) {
-	FILE *fp;
-	if ((fp = fopen("/cdrom/.disk/base_installable", "r"))) {
-		base_on_cd = 1;
-		fclose(fp);
-	}
-	else
-		base_on_cd = 0;
-	return 0;
-}
-
 static int choose_country(void) {
 	if (country)
 		free(country);
@@ -289,11 +277,7 @@ static int get_protocol(void) {
 }
 
 static int choose_suite(void) {
-	/* If the base system can be installed from CD, don't allow to
-	 * select a different suite
-	 */
-	if (! base_on_cd)
-		debconf_input(debconf, "medium", DEBCONF_BASE "suite");
+	debconf_input(debconf, "medium", DEBCONF_BASE "suite");
 	return 0;
 }
 
@@ -531,7 +515,6 @@ int main (void) {
 	/* Use a state machine with a function to run in each state */
 	int state = 0;
 	int (*states[])() = {
-		check_base_on_cd,
 		choose_protocol,
 		get_protocol,
 		choose_country,
