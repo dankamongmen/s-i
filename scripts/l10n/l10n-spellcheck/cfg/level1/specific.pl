12f413950ab32a136e22651b438f0ee8605fa57c
zinosat-guest <zinosat-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2006-04-18 22:19:09 +0000

    Level1-specific script
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@36493 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ scripts/l10n/l10n-spellcheck/cfg/level1/specific.pl
@@ -0,0 +1,50 @@
+#! /usr/bin/perl -w
+
+use strict;
+use Unicode::String;
+
+sub checkSpecials (@)
+{
+    my $msgid = shift;
+    my $msgstr = shift;
+    my $count_id = 1;
+    my $count_st = 1;
+
+    if (defined $msgstr)
+    {
+	return 0 if $msgid =~ /^Choose language$/ && $msgstr !~ /\/[ ]*Choose language$/;
+	return 0 if $msgid =~ /^US\[/ && $msgstr !~ /^[A-Z][A-Z]$/;
+
+	if ($_ =~ m/#. Timezones for /)
+	    {
+		while ($msgid =~ /,/g) { $count_id++ };
+		while ($msgstr =~ /,/g) { $count_st++ };
+		return 0 if ($count_id != $count_st);
+	    }        
+
+	if ($_ =~ m/#. Translators, this is a menu choice. MUST BE UNDER 65 COLUMNS/)
+	    {
+		utf8::decode($msgstr);
+		my $lung = length $msgstr;
+		return 0 if ($lung > 65);
+	    }
+
+    }
+    return 1;
+}
+
+$/ = "\n\n";
+open (PO, "< $ARGV[0]") or die "Unable to open $ARGV[0]: $!\n";
+while (<PO>)
+{
+        s/"\n"//g;
+        next if m/^#, .*fuzzy/m;
+        next unless m/^msgid ".+"$/m;
+        my @msgs = ();
+        while (m/^msg\S+ "(.+)"$/mg)
+        {
+                push (@msgs, $1);
+        }
+        checkSpecials(@msgs) || print;
+}
+close (PO);
