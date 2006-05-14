8818d1955260bfd1ad570df47e4beba64f22f97c
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2006-05-14 03:12:17 +0000

    * Comment out STATE debugging message. * Check for the passwd file before grepping it.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@37301 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/user-setup/functions.sh
+++ packages/user-setup/functions.sh
@@ -3,7 +3,7 @@ is_system_user () {
 	if ! [ -e $ROOT/etc/passwd ]; then
 		return 1
 	fi
-
+	
         # Assume NIS, or any uid from 1000 to 29999,  means there is a user.
         if grep -q '^+:' $ROOT/etc/passwd || \
            grep -q '^[^:]*:[^:]*:[1-9][0-9][0-9][0-9]:' $ROOT/etc/passwd || \
@@ -16,6 +16,10 @@ is_system_user () {
 
 # Returns a true value if root already has a password.
 root_password () {
+	if ! [ -e $ROOT/etc/passwd ]; then
+		return 1
+	fi
+	
 	# Assume there is a root password if NIS is being used.
 	if grep -q '^+:' $ROOT/etc/passwd; then
 		return 0
