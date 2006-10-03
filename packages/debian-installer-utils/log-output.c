409aa50d95a973519a7362cf08645f37641dbcc1
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2006-10-03 22:40:41 +0000

    * log-output: When using --pass-stdout, pass stdout file descriptor   through directly, without the need for a stdout handler function.   Without this, long lines on stdout get broken into 1024-byte pieces.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@41223 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/debian-installer-utils/log-output.c
+++ packages/debian-installer-utils/log-output.c
@@ -12,14 +12,19 @@
 #  define ATTRIBUTE_UNUSED
 #endif
 
-static int pass(const char *buf, size_t len ATTRIBUTE_UNUSED, void *user_data ATTRIBUTE_UNUSED)
+static int close_orig_stdout(pid_t pid ATTRIBUTE_UNUSED, void *user_data)
 {
-	/* di_exec_* always strips any trailing newline, so we have to add
-	 * one back on; we hope that nothing depends on the subsidiary
-	 * program *not* outputting a trailing newline!
-	 */
-	printf("%s\n", buf);
-	fflush(stdout);
+	int orig_stdout = *(int *) user_data;
+	close(orig_stdout);
+	return 0;
+}
+
+static int restore_orig_stdout(pid_t pid ATTRIBUTE_UNUSED, void *user_data)
+{
+	int orig_stdout = *(int *) user_data;
+	if (dup2(orig_stdout, 1) == -1)
+		return 1;
+	close(orig_stdout);
 	return 0;
 }
 
@@ -55,6 +60,10 @@ int main(int argc, char **argv)
 		{ NULL, 0, NULL, 0 }
 	};
 	di_io_handler *stdout_handler = NULL, *stderr_handler = NULL;
+	di_process_handler *parent_prepare_handler = NULL;
+	di_process_handler *child_prepare_handler = NULL;
+	void *prepare_user_data = NULL;
+	int orig_stdout = -1;
 	int status;
 
 	for (;;) {
@@ -79,13 +88,18 @@ int main(int argc, char **argv)
 	if (!argv[optind])
 		return 0;
 
-	if (pass_stdout)
-		stdout_handler = &pass;
-	else
+	if (pass_stdout) {
+		orig_stdout = dup(1);
+		parent_prepare_handler = &close_orig_stdout;
+		child_prepare_handler = &restore_orig_stdout;
+		prepare_user_data = &orig_stdout;
+	} else
 		stdout_handler = &logger;
 	stderr_handler = &logger;
 	status = di_exec_path_full(argv[optind], (const char **) &argv[optind],
-		stdout_handler, stderr_handler, tag, NULL, NULL, NULL, NULL);
+		stdout_handler, stderr_handler, tag,
+		parent_prepare_handler, prepare_user_data,
+		child_prepare_handler, prepare_user_data);
 
 	return di_exec_mangle_status(status);
 }
