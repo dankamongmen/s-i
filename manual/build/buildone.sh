7f4684cbfa24d311002606d9276d15d0c0c21828
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-01-03 07:29:48 +0000

    Document the graphical installer, including the main known issues
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@43782 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- manual/build/buildone.sh
+++ manual/build/buildone.sh
@@ -90,6 +90,7 @@ create_profiled () {
     echo "<!ENTITY smp-config-section \"${smp_config_section}\">" >> $dynamic
     echo "<!ENTITY smp-config-option \"${smp_config_option}\">" >> $dynamic
     echo "<!ENTITY minimum-memory \"${minimum_memory}&notation-megabytes;\">" >> $dynamic
+    echo "<!ENTITY minimum-memory-gtk \"${minimum_memory_gtk}&notation-megabytes;\">" >> $dynamic
 
     sed "s:##SRCPATH##:$source_path:" templates/docstruct.ent >> $dynamic
 
