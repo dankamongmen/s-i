571a349155ce80eb10a7de6b798301499632fdfb
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-01-22 19:14:50 +0000

    Backwards compatibility no longer needed
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@44409 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- manual/build/buildone.sh
+++ manual/build/buildone.sh
@@ -77,8 +77,6 @@ create_profiled () {
     condition="$condition;$optional_paras"
     # Add build options for the manual
     condition="$condition;$unofficial_build;$status;$manual_release;$manual_target"
-    # TODO: Obsoleted, but keep until translations have been updated
-    condition="$condition;common-kpkg;supports-dhcp"
 
     # Write dynamic non-profilable entities into the file
     echo "<!-- arch- and lang-specific non-profilable entities -->" > $dynamic
