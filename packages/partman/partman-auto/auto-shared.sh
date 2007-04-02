3c4de1f3d2ab6db4033a23f775eafe3e0f8f623d
alphix-guest <alphix-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-04-02 20:16:14 +0000

    Allow LVM and device-mapper (i.e. crypto) volumes to be deallocated.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@46112 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/partman/partman-auto/auto-shared.sh
+++ packages/partman/partman-auto/auto-shared.sh
@@ -1,70 +1,50 @@
 ## this file contains a bunch of shared code between partman-auto
 ## and partman-auto-lvm.
 
-# Wipes any traces of LVM from a disk
-# Normally you wouldn't want to use this function, 
-# but wipe_disk() which will also call this function.
-lvm_wipe_disk() {
-	local dev realdev vg pvs pv lv tmpdev restart
+# Gets the major and minor device numbers of a device passed as $1
+# and puts them in $MAJOR and $MINOR.
+dev_get_major_minor() {
+	local dev stats
 	dev="$1"
-	cd $dev
 
-	if [ ! -e /lib/partman/lvm_tools.sh ]; then
-		return 0
-	fi
-
-	. /lib/partman/lvm_tools.sh
-
-	# Check if the device already contains any physical volumes
-	realdev=$(mapdevfs "$(cat $dev/device)")
-	if ! pv_on_device "$realdev"; then
-		return 0
-	fi
-
-	# Ask for permission to erase LVM volumes 
-	db_input critical partman-auto/purge_lvm_from_device
-	db_go || return 1
-	db_get partman-auto/purge_lvm_from_device
-	if [ "$RET" != "true" ]; then
+	if [ -z "$dev" ]; then
 		return 1
 	fi
 
-	# Check all VG's
-	for vg in $(vg_list); do
-		pvs=$(vg_list_pvs $vg)
-		
-		# Only deal with VG's on the selected disk
-		if ! $(echo "$pvs" | grep -q "$realdev"); then
-			continue
-		fi
+	stats=$(ls -al "$dev" | sed -e 's/[[:space:],]\+/ /g')
+	MAJOR=$(echo "$stats" | cut -d' ' -f5)
+	MINOR=$(echo "$stats" | cut -d' ' -f6)
 
-		# Make sure the VG don't span any other disks
-		if $(echo -n "$pvs" | grep -q -v "$realdev"); then
-			log-output -t partman-auto-lvs vgs
-			db_input critical partman-auto/cannot_purge_lvm_from_device || true
-			db_go || true
-			return 1
-		fi
+	if [ -z "$MAJOR" ] || [ -z "$MINOR" ]; then
+		return 1
+	fi
 
-		# Remove LV's from the VG
-		for lv in $(vg_list_lvs $vg); do
-			lv_delete $vg $lv
-		done
+	return 0
+}
 
-		# Remove the VG and its PV's 
-		vg_delete $vg
-		for pv in $pvs; do
-			pv_delete $pv
-		done
-	done
+# Makes sure that parted has no stale info
+dm_update_parted() {
+	local restart tmpdev tmppart realdev
 
-	# Make sure that parted has no stale LVM info
 	restart="0"
 	for tmpdev in $DEVICES/*; do
-		[ -d "$tmpdev" ] || continue
+		for tmppart in $tmpdev/*; do
+			if [ ! -d "$tmppart" ]; then
+				continue
+			fi
+
+			if [ ! -e "$tmppart/crypt_active" ]; then
+				continue
+			fi
+
+			if [ ! -e "$(cat "$tmppart/crypt_active")" ]; then
+				rm -f "$tmppart/crypt_active"
+				rm -f "$tmppart/locked"
+				restart="1"
+			fi
+		done
 
 		realdev=$(cat $tmpdev/device)
-
 		if ! $(echo "$realdev" | grep -q "/dev/mapper/"); then
 			continue
 		fi
@@ -85,14 +65,196 @@ lvm_wipe_disk() {
 	return 0
 }
 
+# Given a lvm device (excluding the /dev/mapper/ part), will gather data on
+# the LVM config, prompt the user and then remove the VG along with all the
+# LV's and PV's which belong to that VG.
+dm_wipe_lvm() {
+	local dev vg pv pvs lv lvs vgtext lvtext pvtext
+	dev="$1"
+
+	# First check that the device exists, it's not an error if it doesn't
+	# since one call to this function can wipe several LV's
+	if [ ! -b "/dev/mapper/$dev" ]; then
+		return 0
+	fi
+
+	if [ ! -e /lib/partman/lvm_tools.sh ]; then
+		return 1
+	fi
+
+	. /lib/partman/lvm_tools.sh
+
+	# We get a device like mainvg-swaplv as $1, let's figure out the VG
+
+	# Make sure that the device includes at least one dash
+        if [ "$(echo -n "$dev" | tr -d -)" = "$dev" ]; then
+                return 1
+	fi
+
+        # Split volume group from logical volume.
+        vg=$(echo ${dev} | sed -e 's#\(.*\)\([^-]\)-[^-].*#\1\2#')
+        # Reduce padded --'s to -'s
+        vg=$(echo ${vg} | sed -e 's#--#-#g')
+	if [ -z "$vg" ]; then
+		return 1
+	fi
+	pvs=$(vg_list_pvs $vg)
+	lvs=$(vg_list_lvs $vg)
+
+	# Ask for permission to erase LVM volumes 
+	vgtext="$vg"
+	pvtext=""
+	for pv in $pvs; do
+		if [ -z "$pvtext" ]; then
+			pvtext="$pv"
+		else
+			pvtext="$pvtext, $pv"
+		fi
+	done
+	lvtext=""
+	for lv in $lvs; do
+		if [ -z "$lvtext" ]; then
+			lvtext="$lv"
+		else
+			lvtext="$lvtext, $lv"
+		fi
+	done
+
+	db_subst partman-auto/purge_lvm_from_device VGTARGETS "$vgtext"
+	db_subst partman-auto/purge_lvm_from_device LVTARGETS "$lvtext"
+	db_subst partman-auto/purge_lvm_from_device PVTARGETS "$pvtext"
+	db_input critical partman-auto/purge_lvm_from_device
+	db_go || return 1
+	db_get partman-auto/purge_lvm_from_device
+	if [ "$RET" != "true" ]; then
+		return 1
+	fi
+
+	# Remove the LV's
+	for lv in $lvs; do
+		lv_delete $vg $lv
+	done
+
+	# Remove the VG
+	vg_delete $vg
+
+	# Remove the PV's
+	for pv in $pvs; do
+		pv_delete $pv
+		partman_unlock_unit $pv
+	done
+
+	return 0
+}
+
+# Given a cryptodev (minus the /dev/mapper/ part), will prompt the user and
+# then remove the device.
+dm_wipe_crypto() {
+	local dev
+	dev="$1"
+
+	# Ask for permission to erase crypto volumes 
+	db_subst partman-auto/purge_crypto_from_device TARGET "$dev"
+	db_input critical partman-auto/purge_crypto_from_device
+	db_go || return 1
+	db_get partman-auto/purge_crypto_from_device
+	if [ "$RET" != "true" ]; then
+		return 1
+	fi
+
+	dmsetup remove "$dev"
+	return $?
+}
+
+# Given a dm device (minus the /dev/mapper/ part), will recursively remove
+# all devices which depend on it and then the device itself.
+dm_wipe_dev() {
+	local dev deps dep name type
+	dev="$1"
+
+	if [ ! -e "/dev/mapper/$dev" ]; then
+		return 0
+	fi
+
+	dev_get_major_minor "/dev/mapper/$dev" || return 1
+	deps=$(dmsetup deps | grep "($MAJOR, $MINOR)" | cut -d: -f1)
+	for dep in $deps; do
+		if [ ! -e "/dev/mapper/$dep" ]; then
+			continue
+		fi
+
+		dev_get_major_minor "/dev/mapper/$dep" || return 1
+		name=$(dmsetup info -j$MAJOR -m$MINOR | grep "Name:" | \
+			cut -d: -f2 | sed -e 's/[[:space:]]//g')
+		if [ -z "$name" ]; then
+			return 1
+		fi
+
+		dm_wipe_dev "$name" || return 1
+	done
+
+	type=$(dmsetup table | grep "^$dev:" | cut -d' ' -f4)
+	if [ -z "$type" ]; then
+		return 1
+	elif [ "$type" = crypt ]; then
+		dm_wipe_crypto "$dev" || return 1
+	else
+		dm_wipe_lvm "$dev" || return 1
+	fi
+
+	return 0
+}
+
+# Wipes any dm devices from a disk (LVM and/or crypto)
+# Given an argument like /dev/hda, /dev/hda1 and /dev/hda2 are also wiped...
+# Normally you wouldn't want to use this function, 
+# but wipe_disk() which will also call this function.
+dm_wipe_disk() {
+	local dev realdev devs check checkdev provide provides restart tmpdev tmppart
+	dev="$1"
+
+	if [ -z "$dev" ]; then
+		return 1
+	fi
+
+	realdev=$(mapdevfs "$(cat $dev/device)")
+	devs=$(ls ${realdev}* 2>/dev/null)
+
+	# Given e.g. /dev/hda we check /dev/hda, /dev/hda1, etc...
+	for check in $devs; do
+		checkdev=$(readlink -f "$check")
+		if [ ! -e "$checkdev" ]; then
+			continue
+		fi
+
+		dev_get_major_minor "$checkdev" || return 1
+
+		provides=$(dmsetup deps | grep "($MAJOR, $MINOR)" | cut -d: -f1)
+
+		if [ -z "$provides" ]; then
+			continue
+		fi
+
+		for provide in $provides; do
+			dm_wipe_dev $provide || return 1
+		done
+	done
+	
+	dm_update_parted || return 1
+	return 0
+}
+
 wipe_disk() {
 	local dev
 	dev="$1"
 
-	lvm_wipe_disk "$dev" || return 1
+	if [ -z "$dev" ]; then
+		return 1
+	fi
 
-	cd $dev
+	dm_wipe_disk "$dev" || return 1
 
+	cd $dev || return 1
 	open_dialog LABEL_TYPES
 	types=$(read_list)
 	close_dialog
