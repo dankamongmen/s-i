83e691221932cc4b9c9420c02e51fd11f924acca
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 20:00:11 +0000

    [project @ Arch-1:colin.watson@canonical.com--2005%kickseed--mainline--0--patch-173] Update timezone to be able to cope with clock-setup and tzsetup udebs.
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-08-05 16:41:35+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47464 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/timezone.sh
+++ packages/kickseed/handlers/timezone.sh
@@ -23,8 +23,14 @@ timezone_handler () {
 
 	if [ "$utc" ]; then
 		ks_preseed base-config tzconfig/gmt boolean true
+		# requires clock-setup
+		ks_preseed d-i clock-setup/utc boolean true
 	else
 		ks_preseed base-config tzconfig/gmt boolean false
+		# requires clock-setup
+		ks_preseed d-i clock-setup/utc boolean false
 	fi
 	ks_preseed base-config tzconfig/preseed_zone string "$1"
+	# requires tzsetup
+	ks_preseed d-i time/zone string "$1"
 }
