f50dada1905380741cb2ca112ea8e3f617d9e5c6
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 19:54:44 +0000

    [project @ Arch-1:colin.watson@canonical.com--2005%kickseed--mainline--0--patch-30] implement rootpw
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-01-25 16:38:07+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47322 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/rootpw.sh
+++ packages/kickseed/handlers/rootpw.sh
@@ -1,8 +1,25 @@
 #! /bin/sh
 
 rootpw_handler () {
-	# TODO REQUIRED [--iscrypted] <password>
-	# requires password preseeding support in shadow (security
-	# considerations), or password question in first stage
-	die "rootpw not supported yet"
+	eval set -- "$(getopt -o '' -l iscrypted -- "$@")" || die_getopt rootpw
+	while :; do
+		case $1 in
+			--iscrypted)
+				die "pre-crypted passwords not yet supported"
+				;;
+			--)	shift; break ;;
+			*)	die_getopt rootpw ;;
+		esac
+	done
+
+	if [ $# -ne 1 ]; then
+		die "rootpw command requires a password"
+	fi
+
+	# requires passwd 1:4.0.3-30.7ubuntu4
+	preseed passwd passwd/root-password password "$1"
+	preseed passwd passwd/root-password-again password "$1"
+	if [ -z "$1" ]; then
+		preseed passwd passwd/root-password-empty note ''
+	fi
 }
