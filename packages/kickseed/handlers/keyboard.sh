2a363046f5ca093b24d498ff855caa2de3ba0276
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 19:57:37 +0000

    [project @ Arch-1:colin.watson@canonical.com--2005%kickseed--mainline--0--patch-104] extra preseed for new Ubuntu keymap selector
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-02-25 20:51:29+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47395 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/keyboard.sh
+++ packages/kickseed/handlers/keyboard.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
 keyboard_handler () {
+	# Specific to Ubuntu keymap selector, but harmless elsewhere.
+	ks_preseed d-i kbd-chooser/method 'Select from full keyboard list'
+
 	ks_preseed d-i console-keymaps-at/keymap 'select' "$1"
 }
