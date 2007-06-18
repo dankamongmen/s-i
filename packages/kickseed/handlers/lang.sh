05966f6c874a4a7cd39386371e7d0b01495d6c2b
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 20:00:06 +0000

    [project @ Arch-1:colin.watson@canonical.com--2005%kickseed--mainline--0--patch-171] Make 'lang' preseed debian-installer/locale as well as preseed/locale, in order to work with localechooser >= 0.07.
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-07-18 17:55:37+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47462 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/lang.sh
+++ packages/kickseed/handlers/lang.sh
@@ -5,5 +5,8 @@ lang_value=
 
 lang_handler () {
 	lang_value="$1"
+	# for localechooser << 0.07; harmless otherwise
 	ks_preseed d-i preseed/locale string "$1"
+	# for localechooser >= 0.07; harmless otherwise
+	ks_preseed d-i debian-installer/locale string "$1"
 }
