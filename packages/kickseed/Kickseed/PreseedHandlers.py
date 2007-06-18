8eff0439b9592856917c4f46761ffab261ed9102
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 19:53:39 +0000

    [project @ Arch-1:colin.watson@canonical.com--2004%kickseed--mainline--0--patch-2] abbreviate slightly
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-01-06 18:11:58+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47292 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/Kickseed/PreseedHandlers.py
+++ packages/kickseed/Kickseed/PreseedHandlers.py
@@ -8,13 +8,16 @@ class PreseedHandlers:
     def __init__(self):
         self.preseeds = []
 
+    def preseed(self, qpackage, qname, qtype, qanswer):
+        self.preseeds.append((qpackage, qname, qtype, qanswer))
+
     def auth(self, args):
         for arg in args:
             if arg == '--enablemd5':
-                self.preseeds.append(('passwd', 'passwd/md5', 'boolean', 'true'))
+                self.preseed('passwd', 'passwd/md5', 'boolean', 'true')
             elif arg == '--useshadow' or arg == '--enableshadow':
                 # TODO: this is true by default already
-                self.preseeds.append(('passwd', 'passwd/shadow', 'boolean', 'true'))
+                self.preseed('passwd', 'passwd/shadow', 'boolean', 'true')
             elif arg == '--enablecache':
                 raise UnimplementedArgument, 'nscd not supported'
             else:
@@ -24,7 +27,7 @@ class PreseedHandlers:
         raise UnimplementedCommand, "autostep makes no sense in d-i"
 
     def keyboard(self, args):
-        self.preseeds.append(('d-i', 'console-keymaps-at/keymap', 'select', args[0]))
+        self.preseed('d-i', 'console-keymaps-at/keymap', 'select', args[0])
 
     def lang(self, args):
-        self.preseed.append(('d-i', 'preseed/locale', 'string', args[0]))
+        self.preseed('d-i', 'preseed/locale', 'string', args[0])
