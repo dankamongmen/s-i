ab28b4b14e9bdc780e5d09ccc9a7433a4e19aed2
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 19:58:26 +0000

    [project @ Arch-1:colin.watson@canonical.com--2005%kickseed--mainline--0--patch-125] Fix kbd-chooser/method preseeding.
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-03-02 19:59:52+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47416 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/keyboard.sh
+++ packages/kickseed/handlers/keyboard.sh
@@ -2,7 +2,8 @@
 
 keyboard_handler () {
 	# Specific to Ubuntu keymap selector, but harmless elsewhere.
-	ks_preseed d-i kbd-chooser/method 'Select from full keyboard list'
+	ks_preseed d-i kbd-chooser/method 'select' \
+		'Select from full keyboard list'
 
 	ks_preseed d-i console-keymaps-at/keymap 'select' "$1"
 }
