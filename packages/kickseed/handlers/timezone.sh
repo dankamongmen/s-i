1d9427a3bb06f9f1fef99ba6cd76550114a98d96
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 19:56:09 +0000

    [project @ Arch-1:colin.watson@canonical.com--2005%kickseed--mainline--0--patch-66] implement timezone, having fixed base-config
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2005-02-07 16:09:48+00:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47358 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/timezone.sh
+++ packages/kickseed/handlers/timezone.sh
@@ -1,8 +1,29 @@
 #! /bin/sh
 
+# requires base-config 2.61ubuntu18
 timezone_handler () {
-	# TODO REQUIRED [--utc] <timezone>
-	# possible to implement as it stands, but very painful; much easier
-	# to fix tzsetup to look at a question in zone.tab format.
-	die "timezone not supported yet"
+	utc=
+
+	eval set -- "$(getopt -o '' -l utc -- "$@")" || die_getopt timezone
+	while :; do
+		case $1 in
+			--utc)
+				utc=1
+				shift
+				;;
+			--)	shift; break ;;
+			*)	die_getopt timezone ;;
+		esac
+	done
+
+	if [ $# -ne 1 ]; then
+		die "timezone command requires a timezone"
+	fi
+
+	if [ "$utc" ]; then
+		ks_preseed base-config tzconfig/gmt boolean true
+	else
+		ks_preseed base-config tzconfig/gmt boolean false
+	fi
+	ks_preseed base-config tzconfig/preseed_zone "$1"
 }
--- packages/kickseed/tests/simple.ks
+++ packages/kickseed/tests/simple.ks
@@ -10,6 +10,7 @@ network --bootproto=static --ip=10.0.2.15 --netmask=255.255.255.0 --gateway=10.0
 part / --size=1024 --grow --maxsize=2048 --asprimary --fstype xfs
 partition swap --size=512 --maxsize=1024
 rootpw rootme
+timezone --utc America/New_York
 url --url http://archive.ubuntu.com/ubuntu
 
 %packages
