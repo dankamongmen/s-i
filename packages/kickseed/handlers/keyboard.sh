78b880386431970244feb129b7084486ba5fd13d
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-06-18 20:01:29 +0000

    [project @ colin.watson@canonical.com-20060922152028-1d0d70ebbfc5b8b2] * Update keyboard handling for console-setup; note that keymap names have   changed from Dapper and earlier, and that layout_variant (e.g. in_guj)   keymap names may be used (closes: Malone #61573).
    Original author: Colin Watson <colin.watson@canonical.com>
    Date: 2006-09-22 16:20:28.143000+01:00
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@47497 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/kickseed/handlers/keyboard.sh
+++ packages/kickseed/handlers/keyboard.sh
@@ -8,4 +8,11 @@ keyboard_handler () {
 	for kbarch in acorn amiga at atari dec mac sun usb; do
 		ks_preseed d-i "console-keymaps-$kbarch/keymap" 'select' "$1"
 	done
+
+	# requires console-setup; keymap names often incompatible with old
+	# kbd-chooser preseeding
+	ks_preseed d-i console-setup/layoutcode string "${1%%_*}"
+	if [ "${1#*_}" != "$1" ]; then
+		ks_preseed d-i console-setup/variantcode string "${1#*_}"
+	fi
 }
