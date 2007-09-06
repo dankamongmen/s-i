22e6d8e99944cf6d1c4eae54c22df1f45f5e5a74
lunar <lunar@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-09-06 10:10:35 +0000

    * Cleanup devfs-style device names from fstab-common and   get_all_partitions().
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@49163 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/partconf/find-parts.c
+++ packages/partconf/find-parts.c
@@ -204,18 +204,7 @@ get_all_partitions(struct partition *parts[], const int max_parts, bool ignore_f
 
             p = malloc(sizeof(*p));
             p->path = ped_partition_get_path(part);
-            if (strstr(p->path, "/dev/ide/") == p->path) {
-                static char *targets[] = { "master", "slave" };
-                int host, bus, target, lun, part;
-
-                if (sscanf(p->path, "/dev/ide/host%d/bus%d/target%d/lun%d/part%d",
-                            &host, &bus, &target, &lun, &part) == 5
-                        && target >= 0 && target <= 1)
-                    asprintf(&p->description, "IDE%d %s\\, part. %d",
-                            2*host + bus + 1, targets[target], part);
-                else
-                    p->description = strdup(p->path);
-            } else if (strstr(p->path, "/dev/hd") == p->path) {
+            if (strstr(p->path, "/dev/hd") == p->path) {
                 static char *targets[] = { "master", "slave" };
                 char drive;
                 int part;
@@ -225,15 +214,6 @@ get_all_partitions(struct partition *parts[], const int max_parts, bool ignore_f
                     asprintf(&p->description, "IDE%d %s\\, part. %d",
                             (drive - 'a') / 2 + 1, targets[(drive - 'a') % 2],
                             part);
-                else
-                    p->description = strdup(p->path);
-            } else if (strstr(p->path, "/dev/scsi/") == p->path) {
-                int host, bus, target, lun, part;
-
-                if (sscanf(p->path, "/dev/scsi/host%d/bus%d/target%d/lun%d/part%d",
-                            &host, &bus, &target, &lun, &part) == 5)
-                    asprintf(&p->description, "SCSI%d (%d\\,%d\\,%d) part. %d",
-                            host + 1, bus, target, lun, part);
                 else
                     p->description = strdup(p->path);
             } else if (strstr(p->path, "/dev/sd") == p->path) {
