67e2264fb5cfc47cf7d16457c4baaf7dddcd507c
waldi <waldi@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-09-11 18:31:20 +0000

    * lib/mklibs/objects_set/lookup.py: Implement lookup. * lib/mklibs/object/__init__.py: Update.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@49249 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ packages/mklibs/lib/mklibs/object/__init__.py
@@ -0,0 +1,3 @@
+def get_object(path):
+    pass
+
--- packages/mklibs/lib/mklibs/objects_set/lookup.py
+++ packages/mklibs/lib/mklibs/objects_set/lookup.py
@@ -1,10 +1,33 @@
 class Lookup(object):
     def __init__(self, path):
+        import os.path
+        if not os.path.exists(path):
+            raise RuntimeError
+
         self.path = path
 
     def find_object(self):
-        raise NotImplementedError
+        return []
 
     def get_objects(self):
-        raise NotImplementedError
+        import os, os.path, mklibs.object
+
+        ret = []
+
+        if os.path.isfile(self.path):
+            obj = mklibs.object.get_object(self.path)
+            if obj is not None:
+                ret.append(obj)
+
+        elif os.path.isdir(self.path):
+            for root, dirs, files in os.walk(self.path):
+                for f in files:
+                    obj = mklibs.object.get_object(os.path.join(root, f))
+                    if obj is not None:
+                        ret.append(obj)
+
+        else:
+            raise NotImplementedError
+
+        return ret
 
