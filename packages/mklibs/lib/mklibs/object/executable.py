ebb953207442105ee25bdf230d6dca63ff66000e
waldi <waldi@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2007-09-18 23:55:47 +0000

    Update.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@49377 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ packages/mklibs/lib/mklibs/object/executable.py
@@ -0,0 +1,3 @@
+class Executable(object):
+    pass
+
---
+++ packages/mklibs/lib/mklibs/object/library.py
@@ -0,0 +1,10 @@
+class Library(object):
+    def get_symbols_exported(self):
+        pass
+
+    def get_symbols_needed(self):
+        pass
+
+def get_library():
+    pass
+
