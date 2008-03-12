80b672371d6ee3eacb17e46a0bd817f6240fc603
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2008-03-12 16:51:56 +0000

    Patch by Matthias Klose stolen from Ubuntu: - iso3166tab.py: Include ErrorPrinter and DefaultHandler from obsolete saxutils.py - drop build dependency on python-xml
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@51889 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/choose-mirror/iso3166tab.py
+++ packages/choose-mirror/iso3166tab.py
@@ -1,16 +1,57 @@
 #!/usr/bin/python
 #
 # Read iso-codes data file and output a .tab file
-# 
+#
 # Copyright (C) 2004 Alastair McKinstry <mckinstry@debian.org>
 # Released under the GPL.
 # $Id: iso3166tab.py,v 1.2 2005/01/08 18:06:46 mckinstry Exp $
 
-from xml.sax import saxutils, make_parser, saxlib, saxexts, ContentHandler
+from xml.sax import _exceptions, handler, make_parser, SAXException, ContentHandler
 from xml.sax.handler import feature_namespaces
 import sys, os, getopt, urllib2
 
-class printLines(saxutils.DefaultHandler):
+class ErrorPrinter:
+    "A simple class that just prints error messages to standard out."
+
+    def __init__(self, level=0, outfile=sys.stderr):
+        self._level = level
+        self._outfile = outfile
+
+    def warning(self, exception):
+        if self._level <= 0:
+            self._outfile.write("WARNING in %s: %s\n" %
+                               (self.__getpos(exception),
+                                exception.getMessage()))
+
+    def error(self, exception):
+        if self._level <= 1:
+            self._outfile.write("ERROR in %s: %s\n" %
+                               (self.__getpos(exception),
+                                exception.getMessage()))
+
+    def fatalError(self, exception):
+        if self._level <= 2:
+            self._outfile.write("FATAL ERROR in %s: %s\n" %
+                               (self.__getpos(exception),
+                                exception.getMessage()))
+
+    def __getpos(self, exception):
+        if isinstance(exception, _exceptions.SAXParseException):
+            return "%s:%s:%s" % (exception.getSystemId(),
+                                 exception.getLineNumber(),
+                                 exception.getColumnNumber())
+        else:
+            return "<unknown>"
+
+class DefaultHandler(handler.EntityResolver, handler.DTDHandler,
+                     handler.ContentHandler, handler.ErrorHandler):
+    """Default base class for SAX2 event handlers. Implements empty
+    methods for all callback methods, which can be overridden by
+    application implementors. Replaces the deprecated SAX1 HandlerBase
+    class."""
+
+
+class printLines(DefaultHandler):
 	def __init__(self, ofile):
 		self.ofile = ofile
 
@@ -19,7 +60,7 @@ class printLines(saxutils.DefaultHandler):
 			return
 		code = attrs.get('alpha_2_code', None)
 		if code == None:
-			raise RunTimeError, "Bad file"	
+			raise RunTimeError, "Bad file"
 		if type(code) == unicode:
 			code = code.encode('UTF-8')
 		name = attrs.get('name', None)
@@ -36,21 +77,21 @@ class printLines(saxutils.DefaultHandler):
 		self.ofile.write (code + '\t' + name + '\n')
 
 
-## 
+##
 ## MAIN
 ##
 
 
 ofile = sys.stdout
 p = make_parser()
-p.setErrorHandler(saxutils.ErrorPrinter())
+p.setErrorHandler(ErrorPrinter())
 try:
 	dh = printLines(ofile)
 	p.setContentHandler(dh)
 	p.parse(sys.argv[1])
 except IOError,e:
 	print in_sysID+": "+str(e)
-except saxlib.SAXException,e:
+except SAXException,e:
 	print str(e)
 
 ofile.close()
