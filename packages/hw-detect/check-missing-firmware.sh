aefd7daad1127ea6f6fcc28e354342ac007f8af7
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2008-06-24 00:51:52 +0000

    only load packages containing missing firmware
    This allows any number of firmware (and other) packages to be dumped onto
    the install media, and d-i only loads the ones it really needs, rather than
    bloating things with everything.
    
    Also, no need to have media-retriever now for check-missing-firmware to
    work; only mountmedia is needed now.
    
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@53871 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/hw-detect/check-missing-firmware.sh
+++ packages/hw-detect/check-missing-firmware.sh
@@ -44,8 +44,25 @@ ask_load_firmware () {
 	fi
 }
 
+listdeb_firmware () {
+	ar p "$1" data.tar.gz | tar zt \
+		| grep '^\./lib/firmware/' \
+		| sed -e 's!^\./lib/firmware/!!'
+}
+
+install_firmware_pkg () {
+	if echo "$1" | grep -q '\.deb$'; then
+		# cache deb for installation into /target later
+		mkdir -p /var/cache/firmware/
+		cp -a "$1" /var/cache/firmware/ || true
+		udpkg --unpack "/var/cache/firmware/$(basename "$1")"
+	else
+		udpkg --unpack "$1"
+	fi
+}
+
 while read_log && ask_load_firmware; do
-	# first, look for loose firmware files on the media
+	# first, look for loose firmware files on the media.
 	if mountmedia; then
 		for file in $files; do
 			if [ -e "/media/$file" ]; then
@@ -57,16 +74,18 @@ while read_log && ask_load_firmware; do
 		umount /media || true
 	fi
 
-	# then try to load udebs (or debs)
+	# Try to load udebs (or debs) that contain the missing firmware.
+	# This does not use anna because debs can have arbitrary
+	# dependencies, which anna might try to install.
 	if mountmedia drivers; then
-		# copy any debs to a holding cache,
-		# for installation into /target later
-		mkdir -p /var/cache/firmware/new/
-		cp -a /media/*.deb /var/cache/firmware/new/ || true
-		if anna media-retriever; then
-			mv /var/cache/firmware/new/* /var/cache/firmware
-			rmdir /var/cache/firmware/new
-		fi
+		grepfor="$(echo "$files" | sed -e 's/ /\n/')"
+		for filename in /media/*.deb /media/*.udeb /media/*.ude; do
+			if [ -f "$filenmame" ]; then
+				if list_deb_firmware "$filename" | grep -qF "$grepfor"; then
+					unstall_firmware_pkg "$filename" || true
+				fi
+			fi
+		done
 		umount /media || true
 	fi
 
