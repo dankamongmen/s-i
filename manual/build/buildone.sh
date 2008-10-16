72cf36dfe8acdff6745db65e65f16f6ae7843ccc
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2008-10-16 07:33:33 +0000

    The kernel version should now always be the same for all architectures
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@56446 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- manual/build/buildone.sh
+++ manual/build/buildone.sh
@@ -81,7 +81,6 @@ create_profiled () {
     echo "<!-- arch- and lang-specific non-profilable entities -->" > $dynamic
     echo "<!ENTITY langext \".${language}\">" >> $dynamic
     echo "<!ENTITY architecture \"${arch}\">" >> $dynamic
-    echo "<!ENTITY kernelversion \"${kernelversion}\">" >> $dynamic
     echo "<!ENTITY arch-listname \"${arch_listname}\">" >> $dynamic
     echo "<!ENTITY altkernelversion \"${altkernelversion}\">" >> $dynamic
     echo "<!ENTITY smp-config-section \"${smp_config_section}\">" >> $dynamic
