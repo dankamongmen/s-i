103f30e25ac639ded1f79fd4772d993264587518
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2009-02-16 12:12:32 +0000

    #include <stdio.h> and <sys/wait.h> in newt-plugin-terminal.c.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@57577 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/cdebconf-terminal/newt-plugin-terminal.c
+++ packages/cdebconf-terminal/newt-plugin-terminal.c
@@ -9,11 +9,13 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <string.h>
 #include <syslog.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <signal.h>
