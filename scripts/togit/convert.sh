98358e0ac4e3200251d3bbccf79993e854d8a507
joeyh <joeyh@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2009-07-24 00:11:20 +0000

    add a partial attempt at scripts for converting to split git repositories
    Currently gets up to r3571
    
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@59670 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ scripts/togit/convert.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = "-r" ]; then
+	lastok=$(egrep '^Exporting revision.*done' log|tail -n 1 | awk '{print $3}')
+	if [ -n "$lastok" ]; then
+		next=$(expr $lastok + 1)
+		inc="--resume-from $next"
+		tee="-a"
+		echo "resuming from revision $next"
+	else
+		echo "resume failure"
+	fi
+fi
+
+if [ ! "$inc" ]; then
+	rm -rf git
+fi
+
+for p in $(grep 'create repository ' d-i.conf | sed 's/create repository //'); do 
+	if [ ! -d git/$p ]; then
+		mkdir  -p git/$p
+		cd git/$p
+		git init --bare
+		cd ../..
+	fi
+done
+
+(
+	cd git
+	../svn-all-fast-export/svn-all-fast-export \
+		--identity-map=../authors-file $inc ../d-i.conf ../svn/d-i
+) | tee $tee log
