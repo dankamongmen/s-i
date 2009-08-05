fd79a96b34d84daf519b9f03ea7bb58fae9e81da
xam <xam@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2009-08-05 17:48:04 +0000

    Correct call to disable_swap. Requires partman-base 134.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@59970 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/partman/partman-md/lib/md-base.sh
+++ packages/partman/partman-md/lib/md-base.sh
@@ -139,7 +139,7 @@ md_prepare () {
 	mkdir -p "$id"
 	local method="$(cat "$id/method" 2>/dev/null || true)"
 	if [ "$method" = swap ]; then
-		disable_swap "$id"
+		disable_swap "$dev" "$id"
 	fi
 	if [ "$method" != raid ]; then
 		echo raid >"$id/method"
