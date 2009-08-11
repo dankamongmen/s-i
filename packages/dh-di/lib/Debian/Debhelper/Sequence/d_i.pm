e379d424e9cf74efd11a6052dfe1fce4db90c1ce
cjwatson <cjwatson@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2009-08-11 08:31:32 +0000

    new debhelper programs wrapping kernel-wedge
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@60099 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/dh-di/lib/Debian/Debhelper/Sequence/d_i.pm
+++ packages/dh-di/lib/Debian/Debhelper/Sequence/d_i.pm
@@ -6,5 +6,8 @@ use strict;
 use Debian::Debhelper::Dh_Lib;
 
 insert_after("dh_install", "dh_di_numbers");
+add_command("dh_di_kernel_gencontrol", "update-control");
+insert_before("dh_testdir", "dh_di_kernel_gencontrol");
+insert_after("dh_install", "dh_di_kernel_install");
 
 1;
