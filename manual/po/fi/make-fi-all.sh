0eb4101632aabfdce876918ce4720028d2d1cfeb
tale <tale@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2009-09-05 14:34:13 +0000

    I'm working on finnish spellchecking using Voikko. This is still work in progress, but to not lose what I have already done I'm storing the scripts I have made.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@60645 48c42b26-1dd6-0310-b98f-a58d8bce7237

---
+++ manual/po/fi/make-fi-all.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+BASEDIR=~/Work/Debian/
+EXTRACT=${BASEDIR}d-i/trunk/scripts/l10n/l10n-spellcheck/extract_msg.pl
+SPELLCHECKER=/usr/bin/enchant
+WORDLIST=${BASEDIR}d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/wls/di_common_wl.txt
+
+if [ -f fi_all.po ] ; then
+    rm fi_all.po
+fi
+
+# no fi_all.po at *.po time
+for f in *.po ; do
+    ${EXTRACT} -msgstr ${f} > $$.temp1
+    tail --lines=+3 $$.temp1 > $$.temp2
+    cut --delimiter=\" --fields=2 < $$.temp2 >> fi_all.po
+done
+
+rm $$.temp1 $$.temp2
+
+# Make list of words tmispell-voikko does not accept
+${SPELLCHECKER} -l -d suomi < fi_all.po > $$.temp3
+sort $$.temp3 | uniq > finnish_unkn_wl.txt
+rm $$.temp3
+echo "Tuntemattomia sanoja " `wc -l finnish_unkn_wl.txt`
