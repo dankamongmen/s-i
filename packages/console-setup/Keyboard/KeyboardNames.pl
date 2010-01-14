8e53913ab2cf82ec078ea6c20e0323ee8e1d4361
zinoviev <zinoviev@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2010-01-14 21:19:21 +0000

    console-setup: release 1.51
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@62001 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/console-setup/Keyboard/KeyboardNames.pl
+++ packages/console-setup/Keyboard/KeyboardNames.pl
@@ -48,6 +48,7 @@ package KeyboardNames;
     'Compaq Internet Keyboard (18 keys)' => 'compaqik18',
     'Compaq Internet Keyboard (7 keys)' => 'compaqik7',
     'Compaq iPaq Keyboard' => 'ipaq',
+    'Creative Desktop Wireless 7000' => 'creativedw7000',
     'DTK2000' => 'dtk2000',
     'Dell' => 'dell',
     'Dell 101-key PC' => 'dell101',
@@ -167,6 +168,11 @@ package KeyboardNames;
     'Trust Direct Access Keyboard' => 'trustda',
     'Trust Slimline' => 'trust_slimline',
     'Trust Wireless Keyboard Classic' => 'trust',
+    'TypeMatrix EZ-Reach 2020' => 'tm2020',
+    'TypeMatrix EZ-Reach 2030 PS2' => 'tm2030PS2',
+    'TypeMatrix EZ-Reach 2030 USB' => 'tm2030USB',
+    'TypeMatrix EZ-Reach 2030 USB (102/105:EU mode)' => 'tm2030USB-102',
+    'TypeMatrix EZ-Reach 2030 USB (106:JP mode)' => 'tm2030USB-106',
     'Unitek KB-1925' => 'unitekkb1925',
     'ViewSonic KU-306 Internet Keyboard' => 'vsonku306',
     'Winbook Model XP5' => 'winbook',
@@ -306,8 +312,8 @@ package KeyboardNames;
 	'Wang model 724 azerty' => 'wang',
     },
     'bg' => {
-	'Standard Phonetic' => 'bas_phonetic',
-	'Traditional Phonetic' => 'phonetic',
+	'New phonetic' => 'bas_phonetic',
+	'Traditional phonetic' => 'phonetic',
     },
     'br' => {
 	'Dvorak' => 'dvorak',
@@ -322,7 +328,7 @@ package KeyboardNames;
     },
     'by' => {
 	'Latin' => 'latin',
-	'Winkeys' => 'winkeys',
+	'Legacy' => 'legacy',
     },
     'ca' => {
 	'English' => 'eng',
@@ -498,6 +504,7 @@ package KeyboardNames;
 	'Malayalam Lalitha' => 'mal_lalitha',
 	'Oriya' => 'ori',
 	'Tamil' => 'tam',
+	'Tamil Keyboard with Numerals' => 'tam_keyboard_with_numerals',
 	'Tamil TAB Typewriter' => 'tam_TAB',
 	'Tamil TSCII Typewriter' => 'tam_TSCII',
 	'Tamil Unicode' => 'tam_unicode',
@@ -513,13 +520,11 @@ package KeyboardNames;
 	'Kurdish, Latin Q' => 'ku',
     },
     'ir' => {
-	'Keypad' => 'keypad',
 	'Kurdish, (F)' => 'ku_f',
 	'Kurdish, Arabic-Latin' => 'ku_ara',
 	'Kurdish, Latin Alt-Q' => 'ku_alt',
 	'Kurdish, Latin Q' => 'ku',
-	'Pro' => 'pro',
-	'Pro Keypad' => 'pro_keypad',
+	'Persian, with Persian Keypad' => 'pes_keypad',
     },
     'is' => {
 	'Dvorak' => 'dvorak',
@@ -531,6 +536,7 @@ package KeyboardNames;
 	'Eliminate dead keys' => 'nodeadkeys',
 	'Georgian' => 'geo',
 	'Macintosh' => 'mac',
+	'US keyboard with Italian letters' => 'us',
     },
     'jp' => {
 	'Kana' => 'kana',
@@ -653,6 +659,7 @@ package KeyboardNames;
 	'Z and ZHE swapped' => 'yz',
     },
     'ru' => {
+	'Bashkirian' => 'bak',
 	'Chuvash' => 'cv',
 	'Chuvash Latin' => 'cv_latin',
 	'DOS' => 'dos',
