2a08edb4699c19a7d7cafc1d19b60dbbb5705d34
fjp <fjp@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2010-02-22 01:06:09 +0000

    Drop --no-recommends option for installation of acpi-related packages
    No longer needed as Recommends are now disabled during base-installer.
    Requires base-installer 1.105.
    
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@62345 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- packages/hw-detect/hw-detect.sh
+++ packages/hw-detect/hw-detect.sh
@@ -483,9 +483,7 @@ fi
 
 # Install acpi
 if [ -d /proc/acpi ]; then
-	apt-install acpi || true
-	# Avoid pulling in console-{common,data} through console-tools
-	apt-install --no-recommends acpid acpi-support-base || true
+	apt-install acpi acpid acpi-support-base || true
 fi
 
 # If hardware has support for pmu, install pbbuttonsd
