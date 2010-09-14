5c9ea7bc060f3dcb139c127abe0b5ee0c9c2507f
sthibault <sthibault@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2010-09-14 22:16:37 +0000

    Fix port URL for non-Linux ports.
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@64716 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- manual/build/buildone.sh
+++ manual/build/buildone.sh
@@ -83,6 +83,7 @@ create_profiled () {
     echo "<!ENTITY architecture \"${arch}\">" >> $dynamic
     echo "<!ENTITY arch-kernel \"${arch_kernel}\">" >> $dynamic
     echo "<!ENTITY arch-listname \"${arch_listname}\">" >> $dynamic
+    echo "<!ENTITY arch-porturl \"${arch_porturl}\">" >> $dynamic
     echo "<!ENTITY altkernelversion \"${altkernelversion}\">" >> $dynamic
     echo "<!ENTITY smp-config-section \"${smp_config_section}\">" >> $dynamic
     echo "<!ENTITY smp-config-option \"${smp_config_option}\">" >> $dynamic
