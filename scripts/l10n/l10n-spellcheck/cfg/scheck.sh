644d1459215a95636eb9b5cfb533c23021d218d1
di-l10n-guest <di-l10n-guest@48c42b26-1dd6-0310-b98f-a58d8bce7237>
2011-01-23 14:41:57 +0000

    Update path
    git-svn-id: svn://svn.debian.org/svn/d-i/trunk@66298 48c42b26-1dd6-0310-b98f-a58d8bce7237

--- scripts/l10n/l10n-spellcheck/cfg/scheck.sh
+++ scripts/l10n/l10n-spellcheck/cfg/scheck.sh
@@ -3,30 +3,30 @@ export PATH=${HOME}/d-i/trunk/scripts/l10n/l10n-spellcheck:$PATH
 
 case "$1" in
 man)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/manual_d-i
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/manual_d-i
 	;;
 l1)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level1
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level1
 	;;
 l2)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level2
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level2
 	;;
 l3)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level3
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level3
 	;;
 l4)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level4
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level4
 	;;
 l5)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level5
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level5
 	;;
 all)
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/manual_d-i
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level1
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level2
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level3
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level4
-	l10n-spellcheck.sh ~/svn/trunk/scripts/l10n/l10n-spellcheck/cfg/level5
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/manual_d-i
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level1
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level2
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level3
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level4
+	l10n-spellcheck.sh ~/d-i/trunk/scripts/l10n/l10n-spellcheck/cfg/level5
 	;;
 *)
 	echo "Unknown parameter. Here's the list of known parameters:"
